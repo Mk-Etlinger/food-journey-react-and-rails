d2c4727c4d131f85e79f52d651dd3a3c00ac656e
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-26 16:54:50 -0400

    Updated css and deleted unnecessary items from App

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MealForm from './containers/MealForm';
 import SymptomForm from './containers/SymptomForm';
 import DateDisplay from './containers/DateDisplay';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      meals: [],
+      symptoms: [],
+    }
+
+
+  }
+  componentDidMount() {
+    fetch('/meals')
+      .then(response => response.json())
+      .then(meals => this.setState({ meals }))
+      .catch(error => console.log("The error is", error))
+  }
+
   render() {
+
+    console.log(this.state.meals)
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-header">        
           <h2>Food Journey</h2>
           <p>A simple food journaling app to get your health on track</p>
         </div>

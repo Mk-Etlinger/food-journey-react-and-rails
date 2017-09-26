7fe0e15a8af5c7afbe61aaeaac84a60cc65083bc
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-26 16:59:16 -0400

    Changed mealType to meal_type

--- client/src/components/RadioInput.js
+++ client/src/components/RadioInput.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default ({ name, mealType, onChangeCb }) => {
+export default ({ name, meal_type, onChangeCb }) => {
     return (
         <div>   
             {name}
             <label htmlFor={name}>
                 <input type="radio" value={name}
-                checked={mealType === name}
+                checked={meal_type === name}
                 onChange={onChangeCb} />                    
             </label>
         </div>

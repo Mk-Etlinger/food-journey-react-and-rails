244fd2a4fb090ed66bfdbf56f4323f30313aed87
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-26 16:58:23 -0400

    Added controllers

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,31 @@
+class IngredientsController < ApplicationController
+
+  def index
+    user_signed_in?
+    authenticate_user!
+    @meal = Meal.find_by(id: params[:meal_id])
+    render json: @meal
+  end
+
+  # def safe
+  #   @ingredient = Ingredient.new
+  # end
+
+  # def mark_safe
+  #   safe_ingredients = ingredient_params[:id]
+  #   @ingredients = Ingredient.where(id: safe_ingredients)
+  #   @ingredients.each { |ingredient| ingredient.update(safe: true) }
+  #   redirect_to dashboard_path
+  # end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(id: [])
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find_by(id: params[])
+  end
+  
+end
---
+++ app/controllers/symptoms_controller.rb
@@ -0,0 +1,80 @@
+class SymptomsController < ApplicationController
+  before_action :user_signed_in?, :authenticate_user!
+  before_action :set_symptom, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @symptoms = current_user.symptoms.order(created_at: :desc)
+  end
+  
+  def new
+    @symptom = Symptom.new
+  end
+  
+  def create
+    redirect_to dashboard_path, alert: no_meals_message and return if valid_meals.empty?
+    @symptom = current_user.symptoms.build(symptom_params)
+
+    if @symptom.save
+      redirect_to symptom_path(@symptom)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @reaction = @symptom.reactions.first
+    respond_to do |format|
+      format.json {render json: @reaction}
+      format.html {}
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if user_authorized? && @symptom.update(symptom_params)
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if user_authorized?
+      @symptom.ingredients.clear
+      @symptom.destroy
+      redirect_to dashboard_path
+    else
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+
+  def set_symptom
+    @symptom = Symptom.find_by(id: params[:id])
+  end
+
+   def symptom_params
+    params.require(:symptom).permit(:description,
+                                    ingredients_attributes: [:current_user_id, :hours_elapsed, :occurred_at],
+                                    reactions_attributes: [:severity, :stress_level, :notes],
+                                    reaction_logs: [:occurred_at])
+  end
+
+  def user_authorized?
+    @symptom.user == current_user
+  end
+
+  def valid_meals
+    hours = symptom_params['ingredients_attributes']['0']['occurred_at'].to_f
+    occurred_at = Time.current.ago(hours.hour)
+    Meal.for_user(current_user).created_within(occurred_at - 3.day, occurred_at)
+  end
+
+  def no_meals_message
+    "Please create a meal first. Currently you have no meals within the selected time period"
+  end
+
+end

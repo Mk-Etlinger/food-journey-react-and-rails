84b16f3730a82ef9d284a8080242e93128e0b179
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-26 17:01:01 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  root 'application#index'
+  get 'dashboard', to: 'dashboard#index'
+  resources :meals do
+    resources :ingredients, only: [:index]
+  end
+
+  resources :symptoms
+  
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # get '/ingredients/safe', to: 'ingredients#safe'
+  # post '/ingredients/mark_safe', to: 'ingredients#mark_safe'
+
+  # get '/users/:id/meals/new', to: 'meals#new', as: 'nested_new_meal'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

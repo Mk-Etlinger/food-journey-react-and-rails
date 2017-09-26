3ff9bd07aee885e2de8a2154b6c3efb720975e34
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-26 16:56:02 -0400

    trouble shooting ingredients_attributes= method

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,27 +3,29 @@ class Meal < ApplicationRecord
   has_many :meal_ingredients
   has_many :ingredients, through: :meal_ingredients
 
-  validates_presence_of :meal_type, :description
+  validates_presence_of :meal_type #, :description
   # validates :description, length: { maximum: 30 }
 
   scope :for_user, ->(user) { where(:user_id => user.id) }
   scope :created_within, ->(time, occurred_at) { where("created_at > ? AND created_at < ?", time, occurred_at) }
 
-  def parse_ingredients(new_ingredients)
-    new_ingredients.split(',').each do |ingredient|
-      self.ingredients << Ingredient.find_or_create_by(name: ingredient.strip.downcase)
-    end
-  end
+  # def parse_ingredients(new_ingredients)
+  #   new_ingredients.split(',').each do |ingredient|
+  #     self.ingredients << Ingredient.find_or_create_by(name: ingredient.strip.downcase)
+  #   end
+  # end
 
   def ingredients_attributes=(ingredients_attributes)
-    new_ingredients = ingredients_attributes.dig('0', 'name')
-    return if new_ingredients.blank?
+    ingredient = Ingredient.find_or_create_by(ingredients_attributes)
+    self.ingredients << ingredient
+  #   new_ingredients = ingredients_attributes.dig('0', 'name')
+  #   return if new_ingredients.blank?
 
-    if new_ingredients.include?(',')
-      parse_ingredients(new_ingredients)
-    else
-      ingredient = Ingredient.find_or_create_by(ingredients_attributes['0'])
-      self.ingredients << ingredient
-    end
+  #   if new_ingredients.include?(',')
+  #     parse_ingredients(new_ingredients)
+  #   else
+  #     ingredient = Ingredient.find_or_create_by(ingredients_attributes['0'])
+  #     self.ingredients << ingredient
+  #   end
   end
 end

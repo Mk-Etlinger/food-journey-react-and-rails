d6c6ba4fbb9fefab4551fa0d12e356ed0384d932
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 12:13:55 -0400

    added FB login link, BrowserRouter, Route, Switch, Link and added the API_URL const

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './App.css';
 import MealForm from './containers/MealForm';
 import SymptomForm from './containers/SymptomForm';
 import DateDisplay from './containers/DateDisplay';
+// import Login from './containers/Login';
+// import post from './ApiCalls';
+
+const API_URL = process.env.REACT_APP_API_URL
 
 class App extends Component {
   constructor() {
@@ -13,8 +19,8 @@ class App extends Component {
       symptoms: [],
     }
 
-
   }
+
   componentDidMount() {
     fetch('/meals')
       .then(response => response.json())
@@ -32,6 +38,8 @@ class App extends Component {
           <h2>Food Journey</h2>
           <p>A simple food journaling app to get your health on track</p>
         </div>
+        <a href='http://localhost:3001/login'>Login with FB</a>
+        {/*<Login />*/}
         <MealForm />
         <SymptomForm />
         <DateDisplay />        

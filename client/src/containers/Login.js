f5e46cbbfc6de614e74ded7f93ca6eeb60d6feb4
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 12:15:28 -0400

    added FB login component

---
+++ client/src/containers/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+class Login extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+        facebook: '',
+        }
+    }
+
+    responseFacebook = (response) => {
+        console.log('Everything coming back from FB:\n',response)
+        return fetch('/auth', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({response})
+            // .then(message => console.log(message))
+            // .then(meal => console.log(meal))
+            // .catch(err => console.log("error of ", err))
+        })
+    }
+        
+
+    render() {
+        return (
+            <div className="Login">        
+                <FacebookLogin
+                appId='502844323394833'
+                autoLoad={false}
+                fields="name,email,picture"
+                onClick={() => console.log('You clicked the FB Login button!')}
+                callback={this.responseFacebook} />               
+            </div>
+        );
+    }
+}
+
+export default Login;

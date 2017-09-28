447389b6021230f52f00f975342fbfba25452cb9
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 11:57:11 -0400

    Implemented omniauth with redirect in test.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
-  def new
+  def create
+  	@user = User.find_or_create_from_auth_hash(auth)
+  	session[:user_id] = @user.id    
+    
+  	redirect_back fallback_location: 'http://localhost:3000'
   end
+
+  def test    
+    redirect_to '/auth/facebook'
+  end  
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to root_path
+  end
+
+  def auth
+    request.env['omniauth.auth']
+  end
+
 end

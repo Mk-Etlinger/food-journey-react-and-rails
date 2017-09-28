59d54242f7f3f29c79bc68a85b51fcc0ac4492da
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 12:07:41 -0400

    deleted ocurred_at attr and added has_many reaction_logs

--- app/serializers/reaction_serializer.rb
+++ app/serializers/reaction_serializer.rb
@@ -1,5 +1,6 @@
 class ReactionSerializer < ActiveModel::Serializer
-  attributes :id, :ingredient_id, :symptom_id, :severity, :stress_level, :notes, :occurred_at
+  attributes :id, :ingredient_id, :symptom_id, :severity, :stress_level, :notes
   belongs_to :ingredient
   belongs_to :symptom
+  has_many :reaction_logs
 end

d021c388c8a542e13215219b04c52a7d9692ac59
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 12:02:23 -0400

    added belongs_to :user, updated reaction_attributes=, set_occurred_at and stubbed out set_user_id

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,16 +1,15 @@
 class Symptom < ApplicationRecord
+  belongs_to :user
   has_many :reactions
   has_many :ingredients, -> { distinct }, through: :reactions
   has_many :reaction_logs, through: :reactions
 
   validates_presence_of :description
 
-  def reactions_attributes=(reaction_attributes)
-    reaction_attributes.values.each do |attribute|
-      self.reactions.each do |reaction|
-        reaction.update(attribute)
-      end
-    end
+  def reactions_attributes=(reaction_attributes)     
+    self.reactions.each do |reaction|
+      reaction.update(reaction_attributes)
+    end    
   end
 
   def ingredients_attributes=(attributes)
@@ -18,7 +17,7 @@ class Symptom < ApplicationRecord
     set_user_id(attributes) && set_occurred_at(attributes)
     set_meals_within_three_days
     @meals.each do |meal|
-      meal.ingredients.each do |ingredient|
+      meal.ingredients.each do |ingredient|      
         self.ingredients << ingredient
       end
     end
@@ -30,16 +29,16 @@ class Symptom < ApplicationRecord
     end
   end
 
-  def set_meals_within_three_days
+  def set_meals_within_three_days    
     @meals = Meal.for_user(user).created_within(@occurred_at - 3.day, @occurred_at)
   end
 
   def set_occurred_at(attributes)
-    hours = attributes['0']['occurred_at'].to_f
+    hours = attributes['occurred_at'].to_f
     @occurred_at = Time.current.ago(hours.hour)
   end
 
   def set_user_id(attributes)
-    self.user_id = attributes.dig('0', 'current_user_id')
+    self.user_id = 1
   end
 end

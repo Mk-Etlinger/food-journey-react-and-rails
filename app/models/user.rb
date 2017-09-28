e0fe7fc8c5f1ef4875b910e5d94f244599af9fc7
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 12:06:04 -0400

    added find_or_create method that takes the oauth hash and creates a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   has_many :meals 
   has_many :symptoms
+
+  def self.find_or_create_from_auth_hash(auth)		
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.name = auth.info.name
+      user.provider = auth.provider
+      user.uid = auth.uid			
+      user.image = auth.info.image
+      user.save!
+		end
+	end
+  
 end

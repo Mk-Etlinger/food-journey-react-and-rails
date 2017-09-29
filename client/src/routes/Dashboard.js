d5853313364a6a9fd8e7dc5c4b5c9a07b9427302
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-29 14:06:16 -0400

    added Dashboard which houses MealForm, SymptomForm and DateDisplay

---
+++ client/src/routes/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MealForm from '../containers/MealForm';
+import SymptomForm from '../containers/SymptomForm';
+import DateDisplay from '../containers/DateDisplay';
+
+export default () => (
+    <div>
+        <MealForm />
+        <SymptomForm />
+        <DateDisplay />        
+    </div>
+)

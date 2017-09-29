f8aeb7cc56cc0d0bfa1d0f2d540054c9b0ecefe9
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-28 20:07:19 -0400

    troubleshooting the facebookLogin React component

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -12,16 +12,16 @@ class Login extends Component {
 
     responseFacebook = (response) => {
         console.log('Everything coming back from FB:\n',response)
-        return fetch('/auth', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({response})
-            // .then(message => console.log(message))
-            // .then(meal => console.log(meal))
-            // .catch(err => console.log("error of ", err))
-        })
+        // return fetch('/auth', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify({response})
+        //     // .then(message => console.log(message))
+        //     // .then(meal => console.log(meal))
+        //     // .catch(err => console.log("error of ", err))
+        // })
     }
         
 
@@ -30,7 +30,8 @@ class Login extends Component {
             <div className="Login">        
                 <FacebookLogin
                 appId='502844323394833'
-                autoLoad={false}
+                size='small'
+                redirectUri='http://localhost:3001/auth/facebook/callback'
                 fields="name,email,picture"
                 onClick={() => console.log('You clicked the FB Login button!')}
                 callback={this.responseFacebook} />               

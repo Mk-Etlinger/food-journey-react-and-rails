cc310e47712e8f2318a0183354e9e60f2f3cb65f
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-29 14:04:58 -0400

    added routes to cleanup App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import './App.css';
-import MealForm from './containers/MealForm';
-import SymptomForm from './containers/SymptomForm';
-import DateDisplay from './containers/DateDisplay';
 // import Login from './containers/Login';
 // import post from './ApiCalls';
-
-const API_URL = process.env.REACT_APP_API_URL
+import Routes from './routes'
 
 class App extends Component {
   constructor() {
@@ -38,11 +33,9 @@ class App extends Component {
           <h2>Food Journey</h2>
           <p>A simple food journaling app to get your health on track</p>
         </div>
-        <a href={`${API_URL}/login`}>Login with FB</a>
-        {/*<Login />*/}
-        <MealForm />
-        <SymptomForm />
-        <DateDisplay />        
+        
+        <Routes />
+        {/*<Login />*/}      
       </div>
     );
   }

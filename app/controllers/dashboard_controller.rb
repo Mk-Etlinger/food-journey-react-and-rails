d2899c704900d322cff4f235defdee79ca953e94
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-29 14:02:09 -0400

    commented out old code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    user_signed_in?
-    authenticate_user!
-    @meals = current_user.meals.order(created_at: :desc).limit(4)
-    @symptoms = current_user.symptoms.order(created_at: :desc).limit(3)
+    # user_signed_in?
+    # authenticate_user!
+    # @meals = current_user.meals.order(created_at: :desc).limit(4)
+    # @symptoms = current_user.symptoms.order(created_at: :desc).limit(3)
   end
 end

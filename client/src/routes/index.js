142423290c20c0c38c84903693058796667700c4
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-29 14:07:21 -0400

    added Router with /, dashboard and overview routes

---
+++ client/src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Overview from './Overview';
+import Navbar from '../components/Navbar'
+
+export default () => (
+    <Router>
+        <div>
+            <Navbar />
+
+            <hr/>
+
+            <Route exact path='/' component={Home} />
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/overview' component={Overview} />
+        </div>
+    </Router>
+)

d80ca58f895489825975eaa04838ea3e3ca0dff0
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-01 18:59:14 -0400

    added meals/symptom state, fetch on ComponentDidMount and added DateDisplay container

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MealForm from '../containers/MealForm';
 import SymptomForm from '../containers/SymptomForm';
 import DateDisplay from '../containers/DateDisplay';
+import Api from '../Api';
 
-export default () => (
-    <div>
-        <MealForm />
-        <SymptomForm />
-        <DateDisplay />        
-    </div>
-)
+export default class Dashboard extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            meals: [],
+            symptoms: [],          
+        }
+    }
+
+    componentDidMount() {
+        // Api.get('meals')
+		let token = "Bearer " + localStorage.getItem("jwt")
+		
+		fetch('/meals', {
+			headers: { "Authorization": token} 
+		})
+			.then(response => response.json())
+			.then(meals => this.setState({ meals }))
+			.catch(error => console.log("The error is", error))
+  }
+
+    render() {
+        return (            
+            <div>
+                <MealForm />
+                <SymptomForm />
+                <DateDisplay meals={this.state.meals} sypmtoms={this.state.symptoms} />        
+            </div>
+        )
+    }
+}

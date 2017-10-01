f684b02742a45a44c77d634848392d72459969a6
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-01 18:53:59 -0400

    updated with notes to continue building out

--- client/src/containers/DateDisplay.js
+++ client/src/containers/DateDisplay.js
@@ -1,35 +1,26 @@
 import React from 'react';
+import DateDropdown from '../components/DateDropdown'
 
-// const data1 = {
-//     date: {
-//         breakfast: ["ham", "cheese"],
-//         lunch: ["salad", "bacon"],
-//         dinner: ["steak", "eggs"]
-//     }
-// }
-// const meal = {
-//     0: {
-//         created_at: "Sept. 24th 2017",
-//         meal_type: "breakfast",
-//         ingredients: [{id: 1, name: "steak"}, {id: 2, name:"eggs"}],
-//     }
-// }
-
-function DateDisplay() {
+function DateDisplay({ meals, symptoms }) {
     const formatting = "  |  "
+    // Send a callback down to change the state in Dashboard
+    const getDates = meals.map(meal => <DateDropdown mealType={meal.meal_type} date={meal.created_at} ingredients={meal.ingredients}/>) 
+    // sort dates by MM-DD-YYYY
+    // iterate through and group meals by date
+    // { 09-27-2017: [
+    //      {meal 1}
+    //      {meal 2}
+    // ]}
+    // 
+    // pass each date obj to props and then map each meal to a button
+
+
     return (
         <div>
-            <h4>Sept. 24th</h4>
-            <div>
-                <button>B</button>
-                <button>L</button>
-                <button>D</button>
-                <button>S</button>
-                {formatting}
-                <button>+</button>
-                <button>+</button>
-            </div>
-            <h4>Sept. 26th</h4>
+            <ul>
+                {getDates}
+            </ul>
+            <h4>Sept. 26th</h4>           
             <div>
                 <button>B</button>
                 <button>L</button>

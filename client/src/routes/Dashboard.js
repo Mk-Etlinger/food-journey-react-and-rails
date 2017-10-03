335d6946ee6d43bfc95b56fda3f4f234d23e9f89
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-03 11:17:06 -0400

    updated fetch to recent_meals and recent_symptoms, commented out Api from import and fixed spelling on line 34

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MealForm from '../containers/MealForm';
 import SymptomForm from '../containers/SymptomForm';
 import DateDisplay from '../containers/DateDisplay';
-import Api from '../Api';
+// import Api from '../Api';
 
 export default class Dashboard extends Component {
     constructor() {
@@ -18,20 +18,26 @@ export default class Dashboard extends Component {
         // Api.get('meals')
 		let token = "Bearer " + localStorage.getItem("jwt")
 		
-		fetch('/meals', {
+		fetch('/recent_meals', {
 			headers: { "Authorization": token} 
 		})
 			.then(response => response.json())
 			.then(meals => this.setState({ meals }))
 			.catch(error => console.log("The error is", error))
-  }
+		fetch('/recent_symptoms', {
+			headers: { "Authorization": token} 
+		})
+			.then(response => response.json())
+			.then(symptoms => this.setState({ symptoms }))
+			.catch(error => console.log("The error is", error))
+    }
 
     render() {
         return (            
             <div>
                 <MealForm />
                 <SymptomForm />
-                <DateDisplay meals={this.state.meals} sypmtoms={this.state.symptoms} />        
+                <DateDisplay meals={this.state.meals} symptoms={this.state.symptoms} />        
             </div>
         )
     }

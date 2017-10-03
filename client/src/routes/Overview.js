e345de21af53d2c32e2e17351e2ff376ad37725f
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-03 11:12:48 -0400

    added Overview class to fetch all Meals and Symptoms

--- client/src/routes/Overview.js
+++ client/src/routes/Overview.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import MealView from '../components/MealView';
 
-export default () => (
-    <div>Overview</div>
-)
+export default class Overview extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            meals: undefined,
+            symptoms: [],
+        }
+    }
+
+    componentDidMount() {
+        // Api.get('meals')
+		let token = "Bearer " + localStorage.getItem("jwt")
+
+		fetch('/meals', {
+			headers: { "Authorization": token} 
+		})
+			.then(response => response.json())
+			.then(meals => this.setState({ meals }))
+			.catch(error => console.log("The error is", error))
+    }
+    
+    render(){  
+        console.log(this.state.meals)      
+        return (
+            <div>
+                Meals:
+                {this.state.meals !== undefined ? <MealView mealsByDate={this.state.meals}/> : ''}
+            </div>
+        )
+    }
+}

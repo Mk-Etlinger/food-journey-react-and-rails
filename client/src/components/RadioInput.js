d509f817c94c1419b25c56b8a9257e3fdf525e0d
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-03 11:32:56 -0400

    formatted input

--- client/src/components/RadioInput.js
+++ client/src/components/RadioInput.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export default ({ name, meal_type, onChangeCb }) => {
+export default ({ name, meal_type, onChangeCb }) => {    
     return (
         <div>   
             {name}
             <label htmlFor={name}>
-                <input type="radio" value={name}
-                checked={meal_type === name}
-                onChange={onChangeCb} />                    
+                <input 
+                    type="radio"                    
+                    value={name}
+                    checked={meal_type === name}
+                    onChange={onChangeCb} />                    
             </label>
         </div>
     )

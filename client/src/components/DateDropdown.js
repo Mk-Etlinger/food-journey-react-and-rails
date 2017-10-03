1787208cd94c14b13d7533736c6737d9bfad1343
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-03 11:31:40 -0400

    refactored with updated format and rendering ShowButton Components

--- client/src/components/DateDropdown.js
+++ client/src/components/DateDropdown.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import ShowButton from './ShowButton';
 
-export default ({ date, mealType, ingredients}) => {
-    console.log(ingredients)
+export default ({ date, meals, symptoms}) => {
+    const formatting = "  |  "
+    const mealButtons = meals.map(meal => <ShowButton clicked={false} key={meal.id} mealId={meal.id} mealType={meal.meal_type} ingredients={meal.ingredients} />)
+    const symptomButtons = symptoms.map(symptom => <ShowButton clicked={false} key={symptom.id} symptomId={symptom.id} />)
     return (
         <div>
             <h4>{date}</h4>
-            <button>{mealType[0]}</button>
-            <ul>
-                {/*ingredients.map(ing => <li>{ing}</li>)*/}
-            </ul>
+            {mealButtons}
+            {formatting}
+            {symptomButtons}
+            <p>_______________</p>
         </div>
     )
 }

477afa7926a1a2df9a952072db9f0484f76ebcd6
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-03 11:35:53 -0400

    cleaned up comments and fixed DateDropdown Rendering

--- client/src/containers/DateDisplay.js
+++ client/src/containers/DateDisplay.js
@@ -2,34 +2,17 @@ import React from 'react';
 import DateDropdown from '../components/DateDropdown'
 
 function DateDisplay({ meals, symptoms }) {
-    const formatting = "  |  "
-    // Send a callback down to change the state in Dashboard
-    const getDates = meals.map(meal => <DateDropdown mealType={meal.meal_type} date={meal.created_at} ingredients={meal.ingredients}/>) 
-    // sort dates by MM-DD-YYYY
-    // iterate through and group meals by date
-    // { 09-27-2017: [
-    //      {meal 1}
-    //      {meal 2}
-    // ]}
-    // 
-    // pass each date obj to props and then map each meal to a button
-
-
+    // Send a callback down to change the state in Dashboard 
+    const mapDates = Object.keys(meals).map((date, i) => {
+        if (symptoms[date]) {
+            return <DateDropdown key={i} date={date} symptoms={symptoms[date]} meals={meals[date]}/>
+        }
+        return <DateDropdown key={i} date={date} symptoms={[]} meals={meals[date]}/>
+    }) 
+    
     return (
         <div>
-            <ul>
-                {getDates}
-            </ul>
-            <h4>Sept. 26th</h4>           
-            <div>
-                <button>B</button>
-                <button>L</button>
-                <button>D</button>
-                <button>S</button>
-                {formatting}
-                <button>+</button>
-                <button>+</button>
-            </div>
+            {mapDates}
         </div>
     )
 }

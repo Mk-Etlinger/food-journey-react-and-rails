a1868037ae8bc33858cbf1de3b20c6fad47e36ce
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-05 09:42:49 -0400

    updated meal init state, componentDidMount to use get actions, added mapStateToProps and display components

--- client/src/routes/Overview.js
+++ client/src/routes/Overview.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
 import MealView from '../components/MealView';
+import SymptomView from '../components/SymptomView';
+import { connect } from 'react-redux';
+import { getMeals } from '../actions/meals';
+import { getSymptoms } from '../actions/symptoms';
 
-export default class Overview extends Component {
+class Overview extends Component {
     constructor() {
         super();
 
         this.state = {
-            meals: undefined,
+            meals: [],
             symptoms: [],
         }
     }
 
     componentDidMount() {
-        // Api.get('meals')
-		let token = "Bearer " + localStorage.getItem("jwt")
-
-		fetch('/meals', {
-			headers: { "Authorization": token} 
-		})
-			.then(response => response.json())
-			.then(meals => this.setState({ meals }))
-			.catch(error => console.log("The error is", error))
-    }
+        // Api.get('meals')		
+		this.props.getMeals();
+		this.props.getSymptoms();
+    };
     
     render(){
         return (
             <div>
-                Meals:
-                {this.state.meals !== undefined ? <MealView mealsByDate={this.state.meals}/> : ''}
+                <div style={{ display: 'inline-block' }}>
+                    <h1>Meals:</h1>
+                    {Object.keys(this.props.meals).length > 0 ? <MealView mealsByDate={this.props.meals}/> : ''}
+                </div>
+                <div style={{ display: 'inline-block', verticalAlign: 'top', marginLeft: 100 }}>
+                    <h1>Symptoms:</h1>
+                    {Object.keys(this.props.meals).length > 0 ? <SymptomView symptomsByDate={this.props.symptoms}/> : ''}
+                </div>
             </div>
         )
-    }
+    };
 }
+
+const mapStateToProps = (state) => {
+    return ({
+        meals: state.meals,
+        symptoms: state.symptoms
+    })
+}
+
+export default connect(mapStateToProps, { getMeals, getSymptoms })(Overview)

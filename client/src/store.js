39c5372b20aa323a7cfc4811446708c2f010a21f
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-05 09:31:59 -0400

    Imported meal/symptom reducers, combined them, applied middleware and connected redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -3,4 +3,23 @@ import {
     applyMiddleware,
     combineReducers
 } from 'redux';
+import meals from './reducers/meals'
+import symptoms from './reducers/symptoms'
+import recentMeals from './reducers/recentMeals'
+import recentSymptoms from './reducers/recentSymptoms'
 import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+    meals,
+    symptoms,
+    recentMeals,
+    recentSymptoms,
+});
+
+const middleware = [thunk];
+
+export default createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(...middleware)
+);

a662fc0efeb9708c6428c370992e493d4858cdaa
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-05 10:52:49 -0400

    added getRecent case

--- client/src/reducers/meals.js
+++ client/src/reducers/meals.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch (action.type) {
         case 'GET_MEALS_SUCCESS':
             return action.meals;
+        case 'GET_RECENT_MEALS_SUCCESS':
+            return action.recentMeals;
         default:
             return state;
     }
--- client/src/reducers/symptoms.js
+++ client/src/reducers/symptoms.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch (action.type) {
         case 'GET_SYMPTOMS_SUCCESS':
             return action.symptoms;
+        case 'GET_RECENT_SYMPTOMS_SUCCESS':
+            return action.recentSymptoms;
         default:
             return state;
     }

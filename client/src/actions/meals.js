a2977bba47c72e1e9e68b214fe66ea4d739396d4
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-05 09:32:33 -0400

    added getMeals to fetch meals and setMeals to update the state with all meals

---
+++ client/src/actions/meals.js
@@ -0,0 +1,24 @@
+const API_URL = process.env.REACT_APP_API_URL
+
+let token = "Bearer " + localStorage.getItem("jwt")
+const setMeals = meals => {
+    return {
+        type: 'GET_MEALS_SUCCESS',
+        meals
+    }
+}
+
+export const getMeals = () => {
+    return dispatch => {
+        return fetch(`${API_URL}/meals`, {
+			headers: { "Authorization": token} 
+		})
+			.then(response => response.json())
+			.then(meals => dispatch(setMeals(meals)))
+			.catch(error => console.log("The error is", error))
+    }
+}
+
+
+
+		

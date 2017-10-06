04c0f7552ab7874ae40df5d40cba45b1c3d56e84
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 12:32:52 -0400

    updated initialState, added CREATE_MEAL_SUCCESS case

--- client/src/reducers/meals.js
+++ client/src/reducers/meals.js
@@ -1,9 +1,19 @@
-export default (state = [], action) => {
+const initialState = {
+    meals: [],
+    symptoms: [],
+    recentMeals: [],
+    recentSymptoms: [],
+}
+
+export default (state = initialState, action) => {
     switch (action.type) {
         case 'GET_MEALS_SUCCESS':
             return action.meals;
         case 'GET_RECENT_MEALS_SUCCESS':
             return action.recentMeals;
+        case 'CREATE_MEAL_SUCCESS':
+            debugger;
+            return Object.assign({}, state, { recentMeals: state.recentMeals.concat(action.meal)})        
         default:
             return state;
     }

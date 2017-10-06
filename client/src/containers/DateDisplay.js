9baff60101daf19356452b09580337eec06694d4
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 12:29:11 -0400

    deleted comments

--- client/src/containers/DateDisplay.js
+++ client/src/containers/DateDisplay.js
@@ -2,7 +2,6 @@ import React from 'react';
 import DateDropdown from '../components/DateDropdown'
 
 function DateDisplay({ meals, symptoms }) {
-    // Send a callback down to change the state in Dashboard
     const mapDates = Object.keys(meals).map((date, i) => {
         if (symptoms[date]) {
             return <DateDropdown key={i} date={date} symptoms={symptoms[date]} meals={meals[date]}/>

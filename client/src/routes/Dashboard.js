14f0ef6e65d909e26cb27424419bd7598e879459
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 12:33:44 -0400

    deleted stale import and added ; to end of 6/7

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -3,9 +3,8 @@ import MealForm from '../containers/MealForm';
 import SymptomForm from '../containers/SymptomForm';
 import DateDisplay from '../containers/DateDisplay';
 import { connect } from 'react-redux';
-import { getRecentMeals } from '../actions/recentMeals'
-import { getRecentSymptoms } from '../actions/recentSymptoms'
-// import Api from '../Api';
+import { getRecentMeals } from '../actions/recentMeals';
+import { getRecentSymptoms } from '../actions/recentSymptoms';
 
 class Dashboard extends Component {
     constructor() {
@@ -24,6 +23,7 @@ class Dashboard extends Component {
     }
 
     render() {
+        console.log(this.props)
         return (            
             <div>
                 <MealForm />

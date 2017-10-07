ed5f2f9ff5aca4fa439488295105fc1a3e6e5f11
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-07 15:04:39 -0400

    added toggleSymptom to handle hiding add symptom button, formatted inputs and added conditional to render button when the symptom button is clicked

--- client/src/containers/MealForm.js
+++ client/src/containers/MealForm.js
@@ -4,6 +4,7 @@ import InputField from '../components/InputField';
 import RadioInput from '../components/RadioInput';
 import { createMeal } from '../actions/createMeal';
 import { updateMealFormData } from '../actions/mealForm';
+import { toggleSymptomButton } from '../actions/toggleSymptomButton';
 
 class MealForm extends Component {
     constructor() {
@@ -27,46 +28,70 @@ class MealForm extends Component {
     }
 
     handleShowForm(e) {
-        this.setState({
-            showForm: true
+        this.setState({ showForm: true })
+        this.props.toggleSymptomButton({
+            active: !this.props.mealFormData.active
         })
     }
 
     handleOnSubmit(e) {
         e.preventDefault();
         this.props.createMeal(this.props.mealFormData)
-        this.setState({
-            showForm: false,           
+        this.setState({ showForm: false })
+        this.props.toggleSymptomButton({
+            active: !this.props.mealFormData.active
         })
     }
 
     render() {
-        const { showForm, meal_type, ingredients, description } = this.props.mealFormData
+        const { meal_type, ingredients } = this.props.mealFormData
+        const { mealFormData } = this.props
         return (
             this.state.showForm === true ?
                 <div>
                     <form onSubmit={this.handleOnSubmit}>                    
-                        <RadioInput value="breakfast" meal_type={meal_type} onChangeCb={this.handleOnChange} />
-                        <RadioInput value="lunch" meal_type={meal_type} onChangeCb={this.handleOnChange} />
-                        <RadioInput value="dinner" meal_type={meal_type} onChangeCb={this.handleOnChange} />
-                        <RadioInput value="snack" meal_type={meal_type} onChangeCb={this.handleOnChange} />
-                        <InputField name="ingredients" type="text" value={ingredients} onChangeCb={this.handleOnChange}/>            
-                        <InputField name="description" type="text" value={description} onChangeCb={this.handleOnChange}/>            
+                        <RadioInput value="breakfast" 
+                            meal_type={meal_type} 
+                            onChangeCb={this.handleOnChange} />
+                        <RadioInput value="lunch" 
+                            meal_type={meal_type} 
+                            onChangeCb={this.handleOnChange} />
+                        <RadioInput value="dinner" 
+                            meal_type={meal_type} 
+                            onChangeCb={this.handleOnChange} />
+                        <RadioInput value="snack" 
+                            meal_type={meal_type} 
+                            onChangeCb={this.handleOnChange} />
+                        <InputField name="ingredients" 
+                            type="text"
+                            value={ingredients}
+                            onChangeCb={this.handleOnChange}
+                            placeholder={"Eg: eggs, bacon, salad"}/>
                         <input type="submit" value="Create Meal"/>
                     </form>
                 </div>
             :
             <div>
-                <button onClick={this.handleShowForm}>Add a meal</button>
+                {mealFormData.active === false ? 
+                    <button onClick={this.handleShowForm}>Add a meal</button>
+                :
+                    ''
+                }
             </div>
+            
         )
     }
 }
 
-const mapeStateToProps = state => {
+const mapStateToProps = state => {
     return {
-        mealFormData: state.mealFormData
+        mealFormData: state.mealFormData,
+        symptomFormData: state.symptomFormData
     }
 }
 
-export default connect(mapeStateToProps, { createMeal, updateMealFormData })(MealForm);
+export default connect(mapStateToProps, { 
+    createMeal,
+    toggleSymptomButton,
+    updateMealFormData 
+})(MealForm);

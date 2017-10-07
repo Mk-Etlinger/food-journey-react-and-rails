12b935f82067fd5f23dd341db81b6526073bac77
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 20:20:09 -0400

    moved to meals reducer

--- client/src/reducers/recentMeals.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {    
-    switch (action.type) {
-        case 'GET_RECENT_MEALS_SUCCESS':
-            return action.recentMeals;
-        default:
-            return state;
-    }
-}
--- client/src/reducers/recentSymptoms.js
+++
@@ -1,8 +0,0 @@
-export default (state = [], action) => {    
-    switch (action.type) {
-        case 'GET_RECENT_SYMPTOMS_SUCCESS':
-            return action.recentSymptoms;
-        default:
-            return state;
-    }
-}

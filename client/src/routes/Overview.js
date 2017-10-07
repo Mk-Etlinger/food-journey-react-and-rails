873c3fabec3889e78095f29ca75f6c6ca45c3fcc
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-07 15:15:41 -0400

    removed state, updated isMealLoaded and SymptomLoaded, added some temp styling

--- client/src/routes/Overview.js
+++ client/src/routes/Overview.js
@@ -6,35 +6,25 @@ import { getMeals } from '../actions/meals';
 import { getSymptoms } from '../actions/symptoms';
 
 class Overview extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            meals: [],
-            symptoms: [],
-        }
-    }
 
     componentDidMount() {
-        // Api.get('meals')		
 		this.props.getMeals();
 		this.props.getSymptoms();
     };
     
     render(){
-        console.log(this.props)
         const { meals, symptoms} = this.props
-        const isMealLoaded = Object.keys(meals).length > 0
-        const isSymptomLoaded = Object.keys(symptoms).length > 0
+        const isMealLoaded = Object.keys(meals.meals).length > 0
+        const isSymptomLoaded = Object.keys(symptoms.symptoms).length > 0
         return (
             <div>
                 <div style={{ display: 'inline-block' }}>
                     <h1>Meals:</h1>
-                    {isMealLoaded && <MealView mealsByDate={meals}/>}
+                    {isMealLoaded && <MealView mealsByDate={meals.meals}/>}
                 </div>
-                <div style={{ display: 'inline-block', verticalAlign: 'top', marginLeft: 100 }}>
+                <div style={{ display: 'inline-block', verticalAlign: 'top', marginLeft: 50 }}>
                     <h1>Symptoms:</h1>
-                    {isSymptomLoaded && <SymptomView symptomsByDate={symptoms}/>}
+                    {isSymptomLoaded && <SymptomView symptomsByDate={symptoms.symptoms}/>}
                 </div>
             </div>
         )

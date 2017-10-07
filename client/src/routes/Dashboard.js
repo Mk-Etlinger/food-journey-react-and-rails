07c86f9270cec80a875f059b2aed98d6ebfe294c
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 20:31:34 -0400

    updated to stateless component

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -1,44 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MealForm from '../containers/MealForm';
 import SymptomForm from '../containers/SymptomForm';
 import DateDisplay from '../containers/DateDisplay';
-import { connect } from 'react-redux';
-import { getRecentMeals } from '../actions/recentMeals';
-import { getRecentSymptoms } from '../actions/recentSymptoms';
 
-class Dashboard extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            recentMeals: [],
-            recentSymptoms: [],
-        }
-    }
-
-    componentDidMount() {
-        // Api.get('meals')
-        this.props.getRecentMeals();
-        this.props.getRecentSymptoms();
-    }
-
-    render() {
-        console.log(this.props)
-        return (            
-            <div>
-                <MealForm />
-                <SymptomForm />
-                <DateDisplay meals={this.props.recentMeals} symptoms={this.props.recentSymptoms} />        
-            </div>
-        )
-    }
+export const Dashboard = () => {
+    return (            
+        <div>
+            <MealForm />
+            <SymptomForm />
+            <DateDisplay />
+        </div>
+    )
 }
-
-const mapStateToProps = (state) => {
-    return ({
-        recentSymptoms: state.recentSymptoms,
-        recentMeals: state.recentMeals
-    })
-}
-
-export default connect(mapStateToProps, { getRecentSymptoms, getRecentMeals })(Dashboard);

5479fb500127e1b72a70875e27bc0ce77d966ebe
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-07 15:14:38 -0400

    updated symptoms: {} initial state, create_symptom to add new meal to dashboard with Object.assign

--- client/src/reducers/symptoms.js
+++ client/src/reducers/symptoms.js
@@ -1,16 +1,22 @@
 const initialState = {
-    symptoms: [],
+    symptoms: {},
     recentSymptoms: {},
 }
 
-export default (state = [], action) => {    
+export default (state = initialState, action) => {    
     switch (action.type) {
         case 'GET_SYMPTOMS_SUCCESS':
-            return action.symptoms;
+            return Object.assign({}, state, { symptoms: action.symptoms })
         case 'GET_RECENT_SYMPTOMS_SUCCESS':            
             return Object.assign({}, state, { recentSymptoms: action.recentSymptoms });
         case 'CREATE_SYMPTOM_SUCCESS':            
-            return Object.assign({}, state, { recentSymptoms: action.recentSymptoms });
+            let key = Object.keys(state.recentSymptoms)[0]
+            let updatedRecentSymptoms = {
+                [key]: Object.values(state.recentSymptoms)[0].concat(action.symptom) 
+            }
+            return Object.assign({}, state, { 
+                recentSymptoms: updatedRecentSymptoms
+            })
         default:
             return state;
     }

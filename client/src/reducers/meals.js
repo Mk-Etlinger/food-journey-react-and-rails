07de6ebc77f10465b4c9b902457b55638c9af95e
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 20:26:55 -0400

    updated initial state, and CREATE_MEAL_SUCCESS to re-render recentMeals and append the newly created meal

--- client/src/reducers/meals.js
+++ client/src/reducers/meals.js
@@ -1,19 +1,33 @@
 const initialState = {
     meals: [],
-    symptoms: [],
-    recentMeals: [],
-    recentSymptoms: [],
+    recentMeals: {},
 }
+
+// {"Oct 6th"=>
+    // [#<Meal:0x007ffea3a78e20
+    // id: 42,
+    // meal_type: "breakfast",
+    // description: "dsf",
+    // user_id: 3,
+    // created_at: Fri, 06 Oct 2017 17:35:12 UTC +00:00,
+    // updated_at: Fri, 06 Oct 2017 17:35:12 UTC +00:00>,]
+// }
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'GET_MEALS_SUCCESS':
             return action.meals;
         case 'GET_RECENT_MEALS_SUCCESS':
-            return action.recentMeals;
+            return Object.assign({}, state, { recentMeals: action.recentMeals })
         case 'CREATE_MEAL_SUCCESS':
-            debugger;
-            return Object.assign({}, state, { recentMeals: state.recentMeals.concat(action.meal)})        
+            // need to handle first meal with conditional
+            let key = Object.keys(state.recentMeals)[0]
+            let updatedRecentMeals = {
+                [key]: Object.values(state.recentMeals)[0].concat(action.meal) 
+            }
+            return Object.assign({}, state, { 
+                recentMeals: updatedRecentMeals
+            })
         default:
             return state;
     }

f2afdf4b81f36408ec0f1968b921b4eee99847e5
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-07 15:10:28 -0400

    added set_meal_form_active case, added active: false to initial state

--- client/src/reducers/mealFormData.js
+++ client/src/reducers/mealFormData.js
@@ -1,13 +1,16 @@
-export default (state = {
-    showForm: false,
+const initialState = {
     meal_type: '',
     ingredients: '',
     description: '',
-}, action) => {
+    active: false,
+}
 
+export default (state = initialState, action) => {
     switch (action.type) {
         case 'UPDATE_MEAL_DATA':
             return action.mealFormData
+        case 'SET_MEAL_FORM_ACTIVE':
+            return Object.assign({}, state, action.toggleMealButton)
         default:
             return state
     }

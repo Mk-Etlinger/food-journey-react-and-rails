f8f62d56ddc118d986b141d43cd2a691a39bc4df
Mike Etlinger <mk-etlinger@gmail.com>
2017-10-06 20:28:07 -0400

    updated Initial state and GET_RECENT/CREATE_SYMPTOM_SUCCESSS

--- client/src/reducers/symptoms.js
+++ client/src/reducers/symptoms.js
@@ -1,9 +1,16 @@
+const initialState = {
+    symptoms: [],
+    recentSymptoms: {},
+}
+
 export default (state = [], action) => {    
     switch (action.type) {
         case 'GET_SYMPTOMS_SUCCESS':
             return action.symptoms;
-        case 'GET_RECENT_SYMPTOMS_SUCCESS':
-            return action.recentSymptoms;
+        case 'GET_RECENT_SYMPTOMS_SUCCESS':            
+            return Object.assign({}, state, { recentSymptoms: action.recentSymptoms });
+        case 'CREATE_SYMPTOM_SUCCESS':            
+            return Object.assign({}, state, { recentSymptoms: action.recentSymptoms });
         default:
             return state;
     }

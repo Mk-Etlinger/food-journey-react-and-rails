4e984418c3682debba81bba5dc652c176434ecb0
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-09 19:33:17 -0400

    updated to handle edge case of no recent meals and meals on a new day

--- client/src/reducers/meals.js
+++ client/src/reducers/meals.js
@@ -3,16 +3,6 @@ const initialState = {
     recentMeals: {},
 }
 
-// {"Oct 6th"=>
-    // [#<Meal:0x007ffea3a78e20
-    // id: 42,
-    // meal_type: "breakfast",
-    // description: "dsf",
-    // user_id: 3,
-    // created_at: Fri, 06 Oct 2017 17:35:12 UTC +00:00,
-    // updated_at: Fri, 06 Oct 2017 17:35:12 UTC +00:00>,]
-// }
-
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'GET_MEALS_SUCCESS':
@@ -20,14 +10,22 @@ export default (state = initialState, action) => {
         case 'GET_RECENT_MEALS_SUCCESS':
             return Object.assign({}, state, { recentMeals: action.recentMeals })
         case 'CREATE_MEAL_SUCCESS':
-            // need to handle first meal with conditional
-            let key = Object.keys(state.recentMeals)[0]
-            let updatedRecentMeals = {
-                [key]: Object.values(state.recentMeals)[0].concat(action.meal) 
-            }
+            const key = Object.keys(action.meal)[0]
+            const recentMealsArray = state.recentMeals[key] || []
+            const updatedRecentMeals = recentMealsArray.length > 0 ? 
+                    {
+                        ...state.recentMeals,
+                        [key]: [...recentMealsArray, action.meal[key]],
+                    }
+                :
+                    {
+                        [key]: [...recentMealsArray, action.meal[key]],
+                        ...state.recentMeals,
+                    }
             return Object.assign({}, state, { 
                 recentMeals: updatedRecentMeals
             })
+            
         default:
             return state;
     }

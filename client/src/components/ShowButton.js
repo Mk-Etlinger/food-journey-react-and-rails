9b766eae99c98375fd41b7dff561d2a19c4a308f
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-09 19:27:20 -0400

    updated destructuring and props logs to console on click

--- client/src/components/ShowButton.js
+++ client/src/components/ShowButton.js
@@ -1,16 +1,28 @@
 import React from 'react';
 // import EditMealForm from '../containers/EditMealForm';
 
-const ShowButton = ({ mealType, ingredients, mealId }) => {
+const ShowButton = ({ 
+    mealId, 
+    mealType, 
+    ingredients, 
+    symptomId, 
+    severity, 
+    stress_level, 
+    ocurred_at 
+}) => {
     // let ingredientLi = ingredients.map(ing => <li key={ing.id}>{ing.name}</li>)
 
     // const editMeal = () => <EditMealForm showForm='true' mealId={mealId} />
     // const handleClick = () => editMeal
-
     return (
         <div style={{ display: 'inline-block' }}>
-            <button onClick={() => console.log("clicked inside ShowButton")}>{mealType !== undefined ? mealType[0] : '+'}</button>
-            {/* edit form needs to show on click */}
+            <button onClick={() => console.log(props)}>
+                {mealType !== undefined ? 
+                    mealType.charAt(0).toUpperCase() 
+                : 
+                    '+'
+                }</button>
+            {/* edit form modal needs to show on click */}
             {/*<EditMealForm showForm={true} mealId={mealId} />*/}
             {/*<ul>
                 {ingredientLi}

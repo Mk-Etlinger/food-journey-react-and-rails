25389ef3e35bc1d54b515ca75ffccb9b9b0985c2
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-09 19:14:47 -0400

    added bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import store from './store'
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
     <Provider store={store}>
-    <App />
+        <App />
     </Provider>, 
     document.getElementById('root'));
 registerServiceWorker();

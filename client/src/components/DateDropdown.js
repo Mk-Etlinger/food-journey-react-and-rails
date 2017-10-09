089989d0a0db39f37b39a5e280f1e820ca6e2239
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-09 19:23:19 -0400

    cleaned up buttons format

--- client/src/components/DateDropdown.js
+++ client/src/components/DateDropdown.js
@@ -3,8 +3,23 @@ import ShowButton from './ShowButton';
 
 export default ({ date, meals, symptoms}) => {
     const formatting = "  |  "
-    const mealButtons = meals.map(meal => <ShowButton clicked={false} key={meal.id} mealId={meal.id} mealType={meal.meal_type} ingredients={meal.ingredients} />)
-    const symptomButtons = symptoms.map(symptom => <ShowButton clicked={false} key={symptom.id} symptomId={symptom.id} />)
+    const mealButtons = meals.map(meal => {
+        return <ShowButton clicked={false} 
+            key={meal.id} 
+            mealId={meal.id} 
+            mealType={meal.meal_type} 
+            ingredients={meal.ingredients}/>
+    })
+    const symptomButtons = symptoms.map(symptom => {
+        return <ShowButton clicked={false} 
+            key={symptom.id} 
+            symptomId={symptom.id} 
+            severity={symptom.reactions[0].severity}
+            stress_level={symptom.reactions[0].stress_level}
+            occurred_at={symptom.reaction_logs[0].occurred_at}
+            ingredients={symptom.ingredients}/>
+    })
+    
     return (
         <div>
             <h4>{date}</h4>

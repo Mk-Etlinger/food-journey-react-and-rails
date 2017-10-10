813e90914798ac8c6f03c7de96fc6a185b9560b4
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-10 18:28:07 -0400

    added updateMeal action

---
+++ client/src/actions/updateMeal.js
@@ -0,0 +1,34 @@
+const API_URL = process.env.REACT_APP_API_URL
+
+let token = "Bearer " + localStorage.getItem("jwt")
+
+const addUpdatedMeal = meal => {
+    debugger;
+    return {
+        type: 'UPDATE_MEAL_SUCCESS',
+        meal
+    }
+}
+
+export const updateMeal = (state) => {
+    return dispatch => {
+        return fetch(`${API_URL}/meals/${state.mealId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            },
+            body: JSON.stringify({ 
+                meal: Object.assign({}, state, { 
+                    ingredients_attributes: 
+                        { 
+                            name: state.ingredients
+                        }
+                    })
+                })
+            })
+        .then(response => response.json())
+        .then(meal => dispatch(addUpdatedMeal(meal)))
+        .catch(err => console.log("error of ", err))
+    }
+}

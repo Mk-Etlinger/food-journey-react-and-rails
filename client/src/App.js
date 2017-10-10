640380ac1757241baf5bbb9b67f3e1664cba4ab6
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-10 10:44:03 -0400

    removed App Header

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">        
-          <h2>Food Journey</h2>
-          <p>A simple food journaling app to get your health on track</p>
-        </div>
         <Routes />
       </div>
     );

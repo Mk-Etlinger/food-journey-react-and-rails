9b6dc29626d91168a282abba4c80a202c3a12994
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-10 10:44:52 -0400

    added a description/title

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -3,6 +3,10 @@ import Login from '../containers/Login';
 
 export default () => (
     <div>
+        <div className="App-header">        
+          <h2>Food Journey</h2>
+          <p>A simple food journaling app to get your health on track</p>
+        </div>
         <Login />
     </div>
 )

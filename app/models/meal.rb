878cece59643c770e5c87492a0984311fb20ad6b
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-10 18:27:46 -0400

    temp fixed ingredients adding instead of replacing old ones, deleted comments

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,8 +3,7 @@ class Meal < ApplicationRecord
   has_many :meal_ingredients, dependent: :destroy
   has_many :ingredients, through: :meal_ingredients
 
-  validates_presence_of :meal_type #, :description
-  # validates :description, length: { maximum: 30 }
+  validates_presence_of :meal_type
 
   scope :for_user, ->(user) { where(:user_id => user.id) }
   scope :created_within, ->(time, occurred_at) { where("created_at > ? AND created_at < ?", time, occurred_at) }
@@ -17,6 +16,7 @@ class Meal < ApplicationRecord
 
   def ingredients_attributes=(ingredients_attributes)
     meal_ingredients = ingredients_attributes['name']
+    self.ingredients.clear
     if meal_ingredients.include?(',')
       parse_ingredients(meal_ingredients)
     else

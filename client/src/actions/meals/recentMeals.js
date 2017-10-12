af9c1145728ea8bdee2f38e6e3f0e723bce26d97
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-11 22:28:03 -0400

    moved into action/meals and changed token to arrow function and

---
+++ client/src/actions/meals/recentMeals.js
@@ -0,0 +1,21 @@
+const API_URL = process.env.REACT_APP_API_URL
+
+let getToken = () => "Bearer " + localStorage.getItem("jwt")
+
+const setRecentMeals = recentMeals => {
+    return {
+        type: 'GET_RECENT_MEALS_SUCCESS',
+        recentMeals
+    }
+}
+
+export const getRecentMeals = () => {
+    return dispatch => {
+        return fetch(`${API_URL}/recent_meals`, {
+			headers: { "Authorization": getToken()} 
+		})
+			.then(response => response.json())
+			.then(recentMeals => dispatch(setRecentMeals(recentMeals)))
+			.catch(error => console.log("The error is", error))
+    }
+}

58692ee3653e6ee3453d8157c167399f6d978401
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-11 22:20:11 -0400

    cleaned up comments and removed user_signed_in? and authenticate_user!

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,22 +1,11 @@
 class IngredientsController < ApplicationController
 
   def index
-    user_signed_in?
-    authenticate_user!
+    # user_signed_in?
+    # authenticate_user!
     @meal = Meal.find_by(id: params[:meal_id])
     render json: @meal
   end
-
-  # def safe
-  #   @ingredient = Ingredient.new
-  # end
-
-  # def mark_safe
-  #   safe_ingredients = ingredient_params[:id]
-  #   @ingredients = Ingredient.where(id: safe_ingredients)
-  #   @ingredients.each { |ingredient| ingredient.update(safe: true) }
-  #   redirect_to dashboard_path
-  # end
 
   private
 

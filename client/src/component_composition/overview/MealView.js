e6cb5bbb718ae2f15ac558e14df95c3d15dd94f9
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-24 11:40:38 -0400

    removed MealCard

--- client/src/component_composition/overview/MealView.js
+++ client/src/component_composition/overview/MealView.js
@@ -4,9 +4,11 @@ import MealCard from './MealCard';
 export default ({ mealsByDate }) => {
     const mapMealDates = Object.keys(mealsByDate).map((date, i) => {       
         let mealsList = mealsByDate[date].map(meal => {
-           return (            
-                   <MealCard key={meal.id} meal={meal} />
-                )
+			return (            
+				<div key={meal.id}>                    
+					<p>{meal.meal_type + ': ' + meal.ingredients.map(ing => ing.name)}</p>                
+				</div>
+			)
         })        
         return (
             <div key={i}>

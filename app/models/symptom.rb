8aa0a0548de337582feb7c135bf2fa7c752e7acf
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-26 14:10:50 -0400

    added an extension to reactions to filter acute/extended, changed to proper validates format, added set_extended_triggers and set_accute_triggers and abstracted out the scope to acute_meals/extended_meals

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,36 +1,60 @@
 class Symptom < ApplicationRecord
   belongs_to :user
   has_many :reactions, dependent: :destroy
-  has_many :ingredients, -> { distinct }, through: :reactions
   has_many :reaction_logs, through: :reactions
+  has_many :ingredients, -> { distinct }, through: :reactions do
+    def acute
+      where("reactions.reaction_type = 'acute'")
+    end
 
-  validates_presence_of :description
+    def extended
+      where("reactions.reaction_type = 'extended'")
+    end
+  end
 
-  def reactions_attributes=(reaction_attributes)     
+  validates :description, presence: true
+
+  def reactions_attributes=(reaction_attributes)
     self.reactions.each do |reaction|
       reaction.update(reaction_attributes)
-    end    
+    end
   end
 
   def ingredients_attributes=(attributes)
     return if self.persisted? # currently not allowing this to update
     set_user_id(attributes) && set_occurred_at(attributes)
-    set_meals_within_three_days
-    @meals.each do |meal|
-      meal.ingredients.each do |ingredient|    
-        self.ingredients << ingredient
-      end
-    end
+    set_acute_triggers && set_extended_triggers
   end
 
-  def reaction_logs=(attributes)
+  def reaction_logs=(_attributes)
     self.reactions.each do |reaction|
       reaction.reaction_logs.create(occurred_at: @occurred_at)
     end
   end
 
-  def set_meals_within_three_days    
-    @meals = Meal.for_user(user).created_within(@occurred_at - 3.day, @occurred_at)
+  def set_extended_triggers
+    extended_meals.each do |meal|
+      meal.ingredients.each do |ingredient|
+        self.ingredients << ingredient
+        self.reactions.last.update(reaction_type: 'extended')
+      end
+    end
+  end
+
+  def set_acute_triggers
+    acute_meals.each do |meal|
+      meal.ingredients.each do |ingredient|
+        self.ingredients << ingredient
+      end
+    end
+  end
+
+  def acute_meals
+    Meal.for_user(user).created_within(@occurred_at - 4.hour, @occurred_at)
+  end
+
+  def extended_meals
+    Meal.for_user(user).created_within(@occurred_at - 3.day, @occurred_at)
   end
 
   def set_occurred_at(attributes)

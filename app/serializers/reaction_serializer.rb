90278a6e68fd22d0fc98ae4e8cb78808a0de752d
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-31 08:42:48 -0400

    added reaction_type

--- app/serializers/reaction_serializer.rb
+++ app/serializers/reaction_serializer.rb
@@ -1,5 +1,5 @@
 class ReactionSerializer < ActiveModel::Serializer
-  attributes :id, :ingredient_id, :symptom_id, :severity, :stress_level, :notes
+  attributes :id, :ingredient_id, :symptom_id, :severity, :stress_level, :notes, :reaction_type
   belongs_to :ingredient
   belongs_to :symptom
   has_many :reaction_logs

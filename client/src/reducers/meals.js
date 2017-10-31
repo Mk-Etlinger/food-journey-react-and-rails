fcee78849b234529a06b7a9e295e6818e04c0307
ETphonehomeslice <mk.etlinger@gmail.com>
2017-10-31 08:47:20 -0400

    simplified update and create

--- client/src/reducers/meals.js
+++ client/src/reducers/meals.js
@@ -1,44 +1,24 @@
 const initialState = {
-    meals: {},
-    recentMeals: {},
+    meals: [],
+    recentMeals: [],
 }
 
 export default (state = initialState, action) => {
-    let date, recentMealsArray;
     switch (action.type) {
         case 'GET_MEALS_SUCCESS':
             return Object.assign({}, state, { meals: action.meals })
         case 'GET_RECENT_MEALS_SUCCESS':
             return Object.assign({}, state, { recentMeals: action.recentMeals })
         case 'UPDATE_MEAL_SUCCESS':
-            date = Object.keys(action.meal)[0]
-            recentMealsArray = state.recentMeals[date].map(meal => {
-                return meal.id !== action.meal[date].id ? meal : action.meal[date]
+            let recentMealsArray = state.recentMeals.map(meal => {
+                return meal.id !== action.meal.id ? meal : action.meal
             });
             
-            return Object.assign({}, state, { 
-                recentMeals: {
-                    ...state.recentMeals,
-                    [date]: recentMealsArray 
-                }
-            })
+            return Object.assign({}, state, { recentMeals: recentMealsArray })
         case 'CREATE_MEAL_SUCCESS':
-            date = Object.keys(action.meal)[0]
-            recentMealsArray = state.recentMeals[date] || []
-            let updatedRecentMeals = recentMealsArray.length > 0 ? 
-                    {
-                        ...state.recentMeals,
-                        [date]: [...recentMealsArray, action.meal[date]],
-                    }
-                :
-                    {
-                        [date]: [...recentMealsArray, action.meal[date]],
-                        ...state.recentMeals,
-                    }
-            return Object.assign({}, state, { 
-                recentMeals: updatedRecentMeals
+          return Object.assign({}, state, {
+                recentMeals: [action.meal, ...state.recentMeals]
             })
-            
         default:
             return state;
     }
--- client/src/reducers/symptoms.js
+++ client/src/reducers/symptoms.js
@@ -1,42 +1,23 @@
 const initialState = {
     symptoms: [],
-    recentSymptoms: {},
+    recentSymptoms: [],
 }
 
 export default (state = initialState, action) => {
-    let date, recentSymptomsArray;  
     switch (action.type) {
         case 'GET_SYMPTOMS_SUCCESS':
             return Object.assign({}, state, { symptoms: action.symptoms })
-        case 'GET_RECENT_SYMPTOMS_SUCCESS':            
+        case 'GET_RECENT_SYMPTOMS_SUCCESS':
             return Object.assign({}, state, { recentSymptoms: action.recentSymptoms });
         case 'UPDATE_SYMPTOM_SUCCESS':
-            date = Object.keys(action.symptom)[0]
-            recentSymptomsArray = state.recentSymptoms[date].map(symptom => {
-                return symptom.id !== action.symptom[date].id ? symptom : action.symptom[date]
+            let updatedSymptomsArray = state.recentSymptoms.map(symptom => {
+                return symptom.id !== action.symptom.id ? symptom : action.symptom
             });
 
+            return Object.assign({}, state, { recentSymptoms: updatedSymptomsArray })
+        case 'CREATE_SYMPTOM_SUCCESS':
             return Object.assign({}, state, { 
-                recentSymptoms: {
-                    ...state.recentSymptoms,
-                    [date]: recentSymptomsArray 
-                }
-            })
-        case 'CREATE_SYMPTOM_SUCCESS':            
-            date = Object.keys(action.symptom)[0]
-            recentSymptomsArray = state.recentSymptoms[date] || []
-            let updatedRecentSymptoms = recentSymptomsArray.length > 0 ? 
-                    {
-                        ...state.recentSymptoms,
-                        [date]: [...recentSymptomsArray, action.symptom[date]],
-                    }
-                :
-                    {
-                        [date]: [...recentSymptomsArray, action.symptom[date]],
-                        ...state.recentSymptoms,
-                    }
-            return Object.assign({}, state, { 
-                recentSymptoms: updatedRecentSymptoms
+                recentSymptoms: [action.symptom, ...state.recentSymptoms]
             })
         default:
             return state;

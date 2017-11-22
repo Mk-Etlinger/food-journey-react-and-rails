810919f9d28bf5c29ddfa37b0c168965b221f683
Mike Etlinger <mk-etlinger@gmail.com>
2017-11-21 23:36:51 -0500

    simplified layout and added temp styling

--- client/src/component_composition/overview/Overview.js
+++ client/src/component_composition/overview/Overview.js
@@ -18,13 +18,13 @@ class Overview extends Component {
         const isMealLoaded = meals.length > 0
         const isSymptomLoaded = symptoms.length > 0
         return (
-            <div style={{ marginTop: 70}}>
-                <div style={{ display: 'inline-block' }}>
-                    <h1>Meals:</h1>
-                    {isMealLoaded && <MealView meals={meals}/>}
+            <div>
+                <div style={{ display: 'inline-block', marginTop: 40 }}>
+                    <h1>Overview</h1>
+                    {isMealLoaded && <MealView meals={meals} symptoms={symptoms}/>}
                 </div>
-                <div style={{ display: 'inline-block', verticalAlign: 'top', marginLeft: 50 }}>
-                    <h1>Symptoms:</h1>
+                <div style={{ display: 'inline-block', verticalAlign: 'top'}}>
+                    {/*<h1>Symptoms:</h1>*/}
                     {isSymptomLoaded && <SymptomView symptomsIndex={symptoms}/>}
                 </div>
             </div>

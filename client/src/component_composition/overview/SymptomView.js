a2de6336d6661886aabe5b00a0545a181f829015
Mike Etlinger <mk-etlinger@gmail.com>
2017-11-26 12:41:46 -0500

    testing Grommet map

--- client/src/component_composition/overview/SymptomView.js
+++ client/src/component_composition/overview/SymptomView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+// import Map from 'grommet/components/Map';
 
 export default ({ symptomsIndex }) => {  
     function triggersFormatter(cell, row) {
@@ -26,6 +27,43 @@ export default ({ symptomsIndex }) => {
                     >Potential Triggers
                 </TableHeaderColumn>
             </BootstrapTable>*/}
+
+            {/*<Map data={{
+            "categories": [
+                {
+                "id": "category-1",
+                "label": "First category",
+                "items": [
+                    {"id": "item-1-1", "label": "First item"},
+                    {"id": "item-1-2", "label": "Second item"},
+                    {"id": "item-1-3", "label": "Third item"}
+                ]
+                },
+                {
+                "id": "category-2",
+                "label": "Second category",
+                "items": [
+                    {"id": "item-2-1", "label": "Fourth item"},
+                    {"id": "item-2-2", "label": "Fifth item"}
+                ]
+                },
+                {
+                "id": "category-3",
+                "label": "Third category",
+                "items": [
+                    {"id": "item-3-1", "label": "Sixth item"},
+                    {"id": "item-3-2", "label": "Seventh item"}
+                ]
+                }
+            ],
+            "links": [
+                {"parentId": "item-1-1", "childId": "item-2-2"},
+                {"parentId": "item-1-2", "childId": "item-2-2"},
+                {"parentId": "item-1-2", "childId": "item-2-1"},
+                {"parentId": "item-2-2", "childId": "item-3-1"},
+                {"parentId": "item-2-1", "childId": "item-3-2"}
+            ]
+            }} />*/}
         </div>
 
     )

51ba86086f0ac27c24153c3ada39fb7a32471f09
Mike Etlinger <mk-etlinger@gmail.com>
2017-12-20 08:55:26 -0500

    extracted form data code

--- client/src/reducers/symptomFormData.js
+++ client/src/reducers/symptomFormData.js
@@ -1,16 +1,9 @@
 const initialState = {
-    description: '',
-    severity: 0,
-    stress_level: 0,
-    notes: '',
-    occurred_at: 0,
-    active: false         
+    active: false,    
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case 'UPDATE_SYMPTOM_DATA':
-            return Object.assign({}, state, action.symptomFormData)
         case 'TOGGLE_SYMPTOM_FORM':     
             return Object.assign({}, state, { active: action.active })
         default:

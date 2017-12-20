99900749e522d4675ddecd54da2b6dd53d7d8b7e
Mike Etlinger <mk-etlinger@gmail.com>
2017-12-20 08:54:52 -0500

    extracted form data code

--- client/src/reducers/mealFormData.js
+++ client/src/reducers/mealFormData.js
@@ -1,14 +1,9 @@
 const initialState = {
-    meal_type: '',
-    ingredients: '',
-    description: '',
     active: false,
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case 'UPDATE_MEAL_DATA':
-            return Object.assign({}, state, action.mealFormData)
         case 'TOGGLE_MEAL_FORM':
             return Object.assign({}, state, { active: action.active })
         default:

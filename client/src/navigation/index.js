8542466641e3c25f9939b5dfa87b3bcb5ebb4549
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-09 09:56:58 -0500

    removed Navbar and placed in layouts, updated layouts to include the proper routes

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from '../component_composition/home/Home';
-import { Dashboard } from '../component_composition/dashboard/Dashboard';
-import Overview from '../component_composition/overview/Overview';
-import Navbar from './Navbar'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Home from '../components/home/Home';
+import User from '../components/home/User';
+import { Dashboard } from '../components/dashboard/Dashboard';
+import Overview from '../components/overview/Overview';
+import { DefaultLayout } from './DefaultLayout';
+import { LoggedInLayout } from './LoggedInLayout';
 
 export default () => (
     <Router>
         <div>
-            <Navbar />
-
-            <Route exact path='/' component={Home} />
-            <Route exact path='/dashboard' component={Dashboard} />
-            <Route exact path='/overview' component={Overview} />
+            <DefaultLayout exact path='/' component={ Home } />
+            <DefaultLayout path='/login' component={ User } />
+            <DefaultLayout path='/register' component={ User } />
+            <LoggedInLayout path='/dashboard' component={ Dashboard } />
+            <LoggedInLayout path='/overview' component={ Overview } />
         </div>
     </Router>
 )

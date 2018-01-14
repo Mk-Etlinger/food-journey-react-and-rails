628da17c06805cf819196eb808d89ce2c27ecf8a
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-14 08:18:40 -0500

    updated to use Grommet Meter and Table

--- client/src/components/overview/Stats.js
+++ client/src/components/overview/Stats.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Grid, Col, Row, Table } from 'react-bootstrap';
+import Table from 'grommet/components/Table';
+import TableRow from 'grommet/components/TableRow';
+import { Grid, Col, Row } from 'react-bootstrap';
 import DisplayChart from '../reusables/DisplayChart';
 
 export default ({ mostSymptomaticFoods }) => {
     const ingredientList = Object.keys(mostSymptomaticFoods).map((food, i) => 
-        <tr key={ i } >
+        <TableRow key={ i } >
             <td>{ i + 1 }</td>
             <td><a href="#">{ food }</a></td> 
-        </tr>
+        </TableRow>
     );
     return (
         <Grid>
             <Row style={{ border: 'solid black' }}>
                 <Col sm={ 3 } md={ 6 }>
-                <Table striped bordered responsive condensed hover>
+                <Table>
                     <thead>
                         <tr>
                             <th>#</th>
@@ -32,3 +34,24 @@ export default ({ mostSymptomaticFoods }) => {
         </Grid>
     );
 }
+
+{/* <Grid>
+            <Row style={{ border: 'solid black' }}>
+                <Col sm={ 3 } md={ 6 }>
+                <Table striped bordered responsive condensed hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Top Trigger Foods</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { ingredientList }
+                    </tbody>
+                </Table>                    
+                </Col>
+                <Col sm={ 3 } md={ 6 }>
+                    <DisplayChart mostSymptomaticFoods={ mostSymptomaticFoods } />
+                </Col>
+            </Row>
+        </Grid> */}

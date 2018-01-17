e6ff0214a12ad6cd05be2dc97f2643bbf68d99cb
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-17 13:27:50 -0500

    updated to class, added layout design via Grommet

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,90 @@
-import React from 'react';
-import MealForm from './MealForm';
-import SymptomForm from './SymptomForm';
-import DateDisplay from './DateDisplay';
+import React, { Component } from 'react';
+import MainContent from './MainContent';
+import PrimaryNavigation from './PrimaryNavigation';
+import Split from 'grommet/components/Split';
+import Box from 'grommet/components/Box';
+import Header from 'grommet/components/Header';
+import Menu from 'grommet/components/Menu';
+import Title from 'grommet/components/Title';
+import Anchor from 'grommet/components/Anchor';
+import UserIcon from 'grommet/components/icons/base/User';
+import ListContainer from '../listView/ListContainer';
+import DataContainer from '../dataView/DataContainer';
+import RelationContainer from '../relationView/RelationContainer';
+import { AddItem } from '../addItem/AddItem';
 
-export const Dashboard = () => {
-    return (            
-        <div style={{ marginTop: 50}}>
+export default class Dashboard extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            activeComp: AddItem,
+            windowWidth: window.innerWidth,
+            windowHeight: window.innerHeight
+        }
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        this.setState({ 
+            windowWidth: window.innerWidth, 
+            windowHeight: window.innerHeight });
+    }
+
+    handleClick = (e) => {
+        const component = componentList[e.target.id]
+        this.setState({
+            activeComp: component
+        })
+    }
+
+    render() {
+        const { windowWidth, activeComp } = this.state
+        const responsiveRender = windowWidth < 718 ? 'xsmall' : 'large'
+
+        return (
             <div>
-                <MealForm />
-                <SymptomForm />
+                <Header size='small'
+                    colorIndex='grey-1'>
+                    <Title margin='small'>
+                        Root Journal
+                    </Title>
+                    <Box flex={ true }
+                        justify='end'
+                        direction='row'
+                        responsive>
+                        <Menu icon={ <UserIcon /> }
+                            dropAlign={{ "right": "right" }}>
+                            <Anchor href='#'
+                                className='active'>
+                                Settings
+                            </Anchor>
+                        </Menu>
+                    </Box>
+                </Header>
+                <Split flex='right'
+                    showOnResponsive='both'
+                    fixed={ false }>
+                        <PrimaryNavigation windowWidth={ windowWidth }
+                            responsiveRender={ responsiveRender }
+                            onClickCB={ this.handleClick }/>
+                        <MainContent activeComp={ activeComp } />
+                </Split>
             </div>
-            <DateDisplay />
-        </div>
-    )
+        )
+    }
 }
+const componentList = {
+    AddItem,
+    ListContainer,
+    DataContainer,
+    RelationContainer
+};

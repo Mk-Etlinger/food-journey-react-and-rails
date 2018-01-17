592b93cecca3041a42c8066cf34ec28a0b181aaa
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-17 13:29:00 -0500

    Deleted the bootstrap Navbar

--- client/src/navigation/LoggedInLayout.js
+++ client/src/navigation/LoggedInLayout.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Navbar from './Navbar';
 import { Route } from 'react-router-dom';
 
-export const LoggedInLayout = ({component: Component, ...props}) => {
+export const LoggedInLayout = ({ component: Component, ...props }) => {
   return (
     <Route {...props} render={matchProps => (
       <div className="LoggedInLayout">
-          <Navbar/>
           <Component {...matchProps} />
       </div>
     )} />

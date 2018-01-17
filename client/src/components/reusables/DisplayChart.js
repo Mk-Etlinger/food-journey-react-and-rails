64aba1476338b66c338e616dac9c5e25057872b5
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-17 13:22:31 -0500

    updated to use Meter

--- client/src/components/reusables/DisplayChart.js
+++ client/src/components/reusables/DisplayChart.js
@@ -1,29 +1,54 @@
 import React from 'react';
-import Chart, { Axis, Grid, Bar, Meter, Label, Box, Base, Value, Layers } from 'grommet/components/chart/Chart';
+import Chart, { Axis, Grid, Bar, Base, Layers } from 'grommet/components/chart/Chart';
 import Legend from 'grommet/components/Legend';
+import Box from 'grommet/components/Box';
+import Value from 'grommet/components/Value';
+import Meter from 'grommet/components/Meter';
+import Label from 'grommet/components/Label';
 
 export default ({ mostSymptomaticFoods = {} }) => {
     
-    // let chartValues = mostSymptomaticFoods.map( (food, i) => {})
     let highestValue = Object.values(mostSymptomaticFoods)[0]
+    let legendLabels = Object.keys(mostSymptomaticFoods).map(( food, i ) => {
+        return { "label": food, "colorIndex": `graph-${ i + 1 }`}
+    })
 
+    let meterValues = Object.keys(mostSymptomaticFoods).map(( food, i ) => {
+        let barValue = mostSymptomaticFoods[food]
+        return (
+            <Box key={ i } 
+                align='center'
+                pad={{ "horizontal": "small", "between": "small" }}>
+                <Value value={ barValue }
+                    active={ false } />
+                <Meter id='meter-1-id'
+                    label={ false }
+                    max={ highestValue + 2 }
+                    min={ 0 }
+                    value={ barValue }
+                    vertical={ true }
+                    active={ false } />
+                <Label margin='small'>
+                { food }
+                </Label>
+            </Box>
+        )
+    })
     return (
-        <Chart full={false}
-            vertical={true}>
-            <Axis count={5}
-                labels={[{"index": 2, "label": `${ highestValue / 2 }`}, {"index": 4, "label": `${ highestValue + 5 }`}]}
-                vertical={true} />
-            <Base height='small'
-                width='small' />
-            <Layers>
-                <Grid rows={5}
-                columns={3} />
-                <Bar values={ Object.values(mostSymptomaticFoods) }
-                    max={ highestValue + 5 }
-                    colorIndex='graph-2' />
-            </Layers>
-            <Axis count={2}
-                labels={[{"index": 0, "label": "2012"}, {"index": 1, "label": "2015"}]} />
-        </Chart>
+        <Chart verticalAlignWith='meter-1-id'>
+            <Axis count={ 5 }
+                labels={[{"index": 2, "label": (highestValue / 2) + 1 }, {"index": 4, "label": highestValue + 2 }]}
+                vertical={ true } />
+            <Chart vertical={ true }
+                verticalAlignWith='meter-1-id'>
+                <Base height='xxsmall'>
+                    { meterValues }
+                </Base>
+                <Layers>
+                <Grid rows={ 5 }
+                    columns={ 3 } />
+                </Layers>
+            </Chart>
+            </Chart>
     )
 }

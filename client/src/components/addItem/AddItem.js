37170e9da7154c102cdef4137aac0d9c0ecda52b
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-23 11:58:13 -0500

    Added grommet Box for Layout

--- client/src/components/addItem/AddItem.js
+++ client/src/components/addItem/AddItem.js
@@ -2,14 +2,42 @@ import React from 'react';
 import MealForm from './MealForm';
 import SymptomForm from './SymptomForm';
 import DateDisplay from './DateDisplay';
+import Box from 'grommet/components/Box';
 
 export const AddItem = () => {
     return (            
-        <div style={{width:'60%' }}>
-            <MealForm />
-            <SymptomForm />
+        <div>
+            <Box
+                direction='row'
+                align='start'
+                justify='center'
+                colorIndex='light-2'>
+                <Box
+                    flex
+                    alignSelf='center'
+                    margin='large'
+                    colorIndex='light-1'> 
+                    <MealForm />
+                </Box>
+                <Box
+                    flex
+                    alignSelf='center'
+                    margin='large'
+                    colorIndex='light-1'> 
+                    <SymptomForm />
+                </Box>
+            </Box>
             
-            <DateDisplay />
+            <Box full
+                justify='center'
+                colorIndex='light-2'>
+                <h2>Recent</h2> 
+                <Box direction='row'
+                    justify='center'
+                    colorIndex='light-2'>
+                    <DateDisplay />
+                </Box>
+            </Box>
         </div>
     )
 }

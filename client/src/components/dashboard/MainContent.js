71a6ff1b2a728779a806bec8f7f50407c9565013
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-23 12:05:04 -0500

    removed temp styling and Box

--- client/src/components/dashboard/MainContent.js
+++ client/src/components/dashboard/MainContent.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import Box from 'grommet/components/Box';
 
 const MainContent = ({ windowWidth, onClickCB, activeComp: ActiveComponent }) => {
-    console.log(ActiveComponent)
     return (
-        <Box
-            justify='start'
-            align='center'
-            pad='medium'
-            size={ splitRightStyling }>
-            <ActiveComponent />
-        </Box>
+        ActiveComponent && <ActiveComponent />
     )
-}
-
-const splitRightStyling = { 
-    height: 'large',
-    width: { min: 'xsmall', max: 'full' }
 }
 
 export default MainContent;

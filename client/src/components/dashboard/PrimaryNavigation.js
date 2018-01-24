0e681aa1d8fd675271abfb394bd736904e1ac4bb
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-23 20:03:55 -0500

    added AnalyticsIcon, BookIcon, AddCircleIcon, InhertIcon from grommet

--- client/src/components/dashboard/PrimaryNavigation.js
+++ client/src/components/dashboard/PrimaryNavigation.js
@@ -1,42 +1,60 @@
 import React from 'react';
 import Columns from 'grommet/components/Columns';
 import Box from 'grommet/components/Box';
+import AnalyticsIcon from 'grommet/components/icons/base/Analytics';
+import BookIcon from 'grommet/components/icons/base/Book';
+import AddCircleIcon from 'grommet/components/icons/base/AddCircle';
+import InheritIcon from 'grommet/components/icons/base/Inherit';
 
 const PrimaryNavigation = ({ windowWidth, onClickCB, responsiveRender }) => {
-
     return (
         <Box colorIndex='neutral-4'
-            justify='start'
-            align='center'
-            size={ {...splitLeftStyling, height: responsiveRender}}>
+            style={{ height: '100%' }}
+            align='stretch'
+            size={ splitLeftStyling }>
             <Columns masonry={ windowWidth < 718 }
                 maxCount={ 1 }
                 size='small'
                 justify='center'>
-                <Box align='center'
-                    alignSelf='stretch'
+                <Box id='AddItem'
                     pad='large'
+                    alignSelf='center'
                     separator='bottom'
-                    colorIndex='neutral-4'>
-                    <a href='#' id='AddItem' onClick={ onClickCB }>Add Item +</a> 
+                    colorIndex='neutral-4'
+                    onClick={ onClickCB } >
+                    <div id='AddItem' onClick={ onClickCB }>
+                        <AddCircleIcon size='medium' />
+                    </div>
                 </Box>
-                <Box align='center'
+                <Box id='ListContainer'
                     pad='large'
+                    alignSelf='center'
                     separator='bottom'
-                    colorIndex='neutral-4'>
-                    <a href='#' id='ListContainer' onClick={ onClickCB }>List View</a>
+                    colorIndex='neutral-4'
+                    onClick={ onClickCB } >
+                    <div id='ListContainer' onClick={ onClickCB }>
+                        <BookIcon size='medium' />
+                    </div>
                 </Box>
-                <Box align='center'
+                <Box id='DataContainer'
                     pad='large'
+                    alignSelf='center'
                     separator='bottom'
-                    colorIndex='neutral-4'>
-                    <a href='#' id='DataContainer' onClick={ onClickCB }>Data View</a>
+                    colorIndex='neutral-4'
+                    onClick={ onClickCB } >
+                    <div id='DataContainer' onClick={ onClickCB }>
+                        <AnalyticsIcon size='medium' />
+                    </div>
                 </Box>
-                <Box align='center'
+                <Box id='RelationContainer' 
                     pad='large'
+                    alignSelf='center'
                     separator='bottom'
-                    colorIndex='neutral-4'>
-                    <a href='#' id='RelationContainer' onClick={ onClickCB }>Relation View</a>
+                    colorIndex='neutral-4'
+                    onClick={ onClickCB } >
+                    <div id='RelationContainer' onClick={ onClickCB }>
+                        <InheritIcon size='medium' />
+                    </div>
                 </Box>
             </Columns>
         </Box>
@@ -44,7 +62,12 @@ const PrimaryNavigation = ({ windowWidth, onClickCB, responsiveRender }) => {
 }
 
 const splitLeftStyling = {
-    width: { min: 'small', max: 'full' }
+    height: 'full',
+    width: 'small',
 }
+
+// const splitLeftStyling = {
+//     width: { min: 'small', max: 'full' }
+// }
 
 export default PrimaryNavigation;

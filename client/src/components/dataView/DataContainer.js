598140c8bc7fb93215340b4939cc583967b1d27e
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-23 20:05:31 -0500

    Updated to use grommet tabs, dynamic render to use array

--- client/src/components/dataView/DataContainer.js
+++ client/src/components/dataView/DataContainer.js
@@ -4,22 +4,27 @@ import { connect } from 'react-redux';
 import { getMeals } from '../../actions/meals/meals';
 import { getSymptoms } from '../../actions/symptoms/symptoms';
 import { getMostSymptomaticFoods } from '../../actions/getMostSymptomaticFoods';
+import Tabs from 'grommet/components/Tabs';
+import Tab from 'grommet/components/Tab';
+import Paragraph from 'grommet/components/Paragraph';
 
 class DataContainer extends Component {
     constructor() {
         super()
 
         this.state = {
-            compActive: 'overview',
+            ActiveComp: DataView,
+            activeIndex: 0
         }
 
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleClick = (e) => {
-        const component = componentList[e.target.id]
+        const component = componentList[e]
         this.setState({
-            activeComp: component
+            ActiveComp: component,
+            activeIndex: e
         })
     }
 
@@ -33,15 +38,21 @@ class DataContainer extends Component {
         const { meals } = this.props.meals, 
             { symptoms } = this.props.symptoms,
             { mostSymptomaticFoods } = this.props.overviewQueries,
-            { compActive } = this.state
+            { ActiveComp } = this.state
         const isMealLoaded = meals.length > 0,
             isSymptomLoaded = symptoms.length > 0,
             isMostSymptomaticFoodsLoaded = Object.keys(mostSymptomaticFoods).length > 0
 
         return (
             <div>
-                { isMostSymptomaticFoodsLoaded && 
-                    <DataView mostSymptomaticFoods={mostSymptomaticFoods}/> }
+                <Tabs activeIndex={ this.state.activeIndex } 
+                    onActive={ this.handleClick }>
+                    <Tab id='IndexView' title='Top Triggers'>
+                        { isMostSymptomaticFoodsLoaded && <ActiveComp 
+                            mostSymptomaticFoods={ mostSymptomaticFoods }/> 
+                        }
+                    </Tab>
+                </Tabs>
             </div>
         )
     };
@@ -50,8 +61,9 @@ class DataContainer extends Component {
 DataContainer.defaultProps = {
     mostSymptomaticFoods: {},
 }
-const componentList = {
-};
+const componentList = [
+    DataView
+];
 
 const mapStateToProps = (state) => {
     return ({

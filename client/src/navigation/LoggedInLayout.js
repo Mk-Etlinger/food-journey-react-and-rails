abe1c40bc7224736a2a3388c9a78eb2f38a1bda8
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-23 20:12:25 -0500

    updated format

--- client/src/navigation/LoggedInLayout.js
+++ client/src/navigation/LoggedInLayout.js
@@ -3,9 +3,9 @@ import { Route } from 'react-router-dom';
 
 export const LoggedInLayout = ({ component: Component, ...props }) => {
   return (
-    <Route {...props} render={matchProps => (
+    <Route { ...props } render={ matchProps => (
       <div className="LoggedInLayout">
-          <Component {...matchProps} />
+          <Component { ...matchProps } />
       </div>
     )} />
   )

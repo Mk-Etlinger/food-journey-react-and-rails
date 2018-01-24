50e65840b9e79903c099169b7cc0280d4f12849a
Mike Etlinger <mk-etlinger@gmail.com>
2018-01-23 20:11:38 -0500

    updated dumb comp/container convention

---
+++ client/src/components/reusables/TODO/MealForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import InputField from '../reusables/InputField';
+import ToggleRadioButtons from '../reusables/ToggleRadioButtons';
+import Form from 'grommet/components/Form';
+import Button from 'grommet/components/Button';
+import AddIcon from 'grommet/components/icons/base/Add';
+
+export const MealForm = ({ onSubmitCB, onChangeCb, formData, hideForm }) => {
+    const { meal_type, ingredients } = formData
+    console.log(ingredients)
+    return (
+        <div style={ divStyle }>
+            <span onClick={ hideForm } style={{ margin: '0 0 99% 97%', cursor: 'pointer' }}>x</span>
+            <Form onSubmit={ onSubmitCB }>
+                <ToggleRadioButtons
+                    currentValue={ meal_type }
+                    name='meal_type'
+                    fields={ ['breakfast', 'lunch', 'dinner', 'snack'] }
+                    onChangeCb={ this.handleRadioOnChange }/>
+                <InputField name="ingredients" 
+                    type="text"
+                    value={ ingredients }
+                    onChangeCb={ onChangeCb }
+                    placeholder={ "Eg: eggs, bacon, salad" }/>
+                <input type="submit" value="Create Meal"/>
+            </Form>
+        </div>
+    )
+}
+
+const divStyle = { 
+    border: '1px solid  #d8edf3', 
+    boxShadow: '2px 6px 6px grey', 
+    borderRadius: '4px' , 
+    padding: '20px'
+}

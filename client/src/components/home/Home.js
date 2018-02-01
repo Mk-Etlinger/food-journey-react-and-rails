b47181396e01c62b2c6170c814faea422d432b44
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-01 16:15:58 -0500

    Imported Hero-heroImage from Grommet, removed handleClick fn

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Hero from 'grommet/components/Hero';
+import Heading from 'grommet/components/Heading';
+import Box from 'grommet/components/Box';
+import Button from 'grommet/components/Button';
+import Image from 'grommet/components/Image';
+import LoginIcon from 'grommet/components/icons/base/Login';
+import Card from 'grommet/components/Card';
+import heroImage from '../../images/food_journey_landing_photo_large.jpg';
 
 export default class Home extends Component {
-    handleClick = (e) => {
-        const route = e.target.id
-        this.props.history.push(`/${ route }`)
-    }
 
     checkAuth()  {
         // when added to redux store
@@ -18,14 +22,34 @@ export default class Home extends Component {
 
     render() {        
         return (
-            <div>
-                <div className="App-header">        
-                <h2>Food Journey</h2>
-                <p>A simple food journaling app to get your health on track</p>
-                </div>
-                <button id='login' onClick={(e) => this.handleClick(e)}>Login</button>
-                <button id='register' onClick={(e) => this.handleClick(e)}>Register</button>
-            </div>
+            <Hero background={ <Image src={ heroImage }
+                full
+                align={{ "bottom": true }}
+                fit='cover' />} 
+                backgroundColorIndex='dark'>
+                <Box direction='row'
+                    justify='center'
+                    align='center'>
+                    <Box basis='1/2'
+                        align='start'
+                        pad='medium' />
+                    <Box flex 
+                        basis='1/2'
+                        justify='start'
+                        align='center'
+                        margin={{ bottom: 'large' }}>
+                        <Card margin='small' 
+                            textSize='medium'
+                            heading='Root Journal'
+                            description='A simple food journaling app to get your health on track' />
+                        <Button icon={ <LoginIcon /> }
+                            plain
+                            label='Login / Register'
+                            path='/login' />
+                            
+                    </Box>
+                </Box>
+            </Hero>
         )
     }
 }

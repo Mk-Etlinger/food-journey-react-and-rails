481dcf12e1950451c0fb7710f60137db0b4e4c74
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-01 16:29:07 -0500

    cleaned up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,16 @@ Rails.application.routes.draw do
   root 'application#index'
   get 'dashboard', to: 'dashboard#index'
   get 'login', to: 'sessions#login'
-  get 'auth/facebook/callback', to: 'sessions#create'
   get 'recent_meals', to: 'meals#recent'
   get 'recent_symptoms', to: 'symptoms#recent'
   get 'most_symptomatic_ingredients', to: 'symptoms#most_symptomatic_ingredients'
+  
+  resources :symptoms
   resources :meals do
     resources :ingredients, only: [:index]
   end
 
-  resources :symptoms
-
-  Rails.application.routes.draw do    
-    resources :meals
-    post 'user_token' => 'user_token#create'
-  end  
-  
-
-  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # get '/ingredients/safe', to: 'ingredients#safe'
-  # post '/ingredients/mark_safe', to: 'ingredients#mark_safe'  
+  post 'user_token' => 'user_token#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 

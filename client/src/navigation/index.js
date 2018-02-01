21f007174f052a8222f6c300e4a82bd7648e4de1
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-01 16:28:00 -0500

    removed unused routes

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Home from '../components/home/Home';
 import User from '../components/home/User';
 import Dashboard from '../components/dashboard/Dashboard';
-import Overview from '../components/overview/Overview';
 import { DefaultLayout } from './DefaultLayout';
 import { LoggedInLayout } from './LoggedInLayout';
 
@@ -12,9 +11,7 @@ export default () => (
         <div>
             <DefaultLayout exact path='/' component={ Home } />
             <DefaultLayout path='/login' component={ User } />
-            <DefaultLayout path='/register' component={ User } />
             <LoggedInLayout path='/dashboard' component={ Dashboard } />
-            <LoggedInLayout path='/overview' component={ Overview } />
         </div>
     </Router>
 )

029f8a7a96ddd8af41700dd021ff99ce6b9939a8
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-01 16:06:52 -0500

    cleaned up comments

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,8 +1,6 @@
 class IngredientsController < ApplicationController
 
   def index
-    # user_signed_in?
-    # authenticate_user!
     @meal = Meal.find_by(id: params[:meal_id])
     render json: @meal
   end

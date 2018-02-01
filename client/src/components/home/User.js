0588fabd836bb044332c94d1546f151e1a433736
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-01 16:22:19 -0500

    simplified component, removed handleClick, added register functionality

--- client/src/components/home/User.js
+++ client/src/components/home/User.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Form } from 'react-bootstrap';
 import Login from './Login';
-import Register from './Register';
+import Box from 'grommet/components/Box';
 
 class User extends Component {
     constructor() {
@@ -11,11 +10,10 @@ class User extends Component {
             activeComp: window.location.pathname,
             email: '',
             password: '',
-            passwordCheck: '',
         }
         
-        this.handleInputChange = this.handleInputChange.bind(this)
         this.handleOnSubmit = this.handleOnSubmit.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
         this.checkAuth = this.checkAuth.bind(this)    
     }
         
@@ -24,11 +22,6 @@ class User extends Component {
         this.setState({
             [name]: value
         })
-    }
-
-    handleClick = (e) => {
-        const route = e.target.id
-        this.props.history.push(`/${ route }`)
     }
 
     checkAuth()  {
@@ -45,10 +38,13 @@ class User extends Component {
         e.preventDefault();
         const email = this.state.email,
             password = this.state.password,
+            register = e.currentTarget.id === 'register',
             request = JSON.stringify({
-                auth: { email: email, password: password }
+                auth: { email: email, password: password, register: register }
             })
+        
         const API_URL = process.env.REACT_APP_API_URL
+        
         this.setState({ email: '', password: ''})
         return fetch(`${API_URL}/user_token`, {
             method: "POST",
@@ -65,55 +61,20 @@ class User extends Component {
 
     render() {
 
-        const { email, 
-                password, 
-                passwordCheck, 
-                activeComp 
-        } = this.state
+        const {  activeComp } = this.state
 
         const loginIsActive = activeComp === '/login'
         
         return (
-            <div>
-                <h1 id='login' 
-                    onClick={(e) => this.handleClick(e)} 
-                    style={h1Style}>
-                    Login
-                </h1>
-                <h1 style={{ display: 'inline' }}> | </h1>
-                <h1 id='register' 
-                    onClick={(e) => this.handleClick(e)} 
-                    style={h1Style}>
-                    Register
-                </h1>
-                <Form inline onSubmit={ this.handleOnSubmit }>
-                { loginIsActive ?
-                    <div className="login">
-                        <Login email={ email } 
-                            password={ password }
-                            onChangeCb={ this.handleInputChange }
-                            onSubmitCb={ this.handleOnSubmit }/>
-                    </div>
-                :
-                    <div className="register">
-                        <Register email={ email }
-                            password={ password }
-                            passwordCheck={ passwordCheck }
-                            onChangeCb={ this.handleInputChange }
-                            onSubmitCb={ this.handleOnSubmit }/>
-                    </div>
-                }
-                    <input type="submit" 
-                        value={ loginIsActive ? 'Login' : 'Register' }/>
-                </Form>
-            </div>
+            <Box direction='row' 
+                justify='center'>      
+                <Login email={ this.state.email }
+                    password={ this.state.password }
+                    onChangeCB={ this.handleInputChange }
+                    onSubmitCB={ this.handleOnSubmit } />
+            </Box>
         );
     }
-}
-
-const h1Style = { 
-    display: 'inline',
-    cursor: 'pointer' 
 }
 
 export default User;

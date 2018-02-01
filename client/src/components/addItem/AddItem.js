de3884b6dc46183aeecdf4d9e0f3130a53b6a66f
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-01 16:09:51 -0500

    cleaned up comments, added box full and another box flex

--- client/src/components/addItem/AddItem.js
+++ client/src/components/addItem/AddItem.js
@@ -6,30 +6,32 @@ import Box from 'grommet/components/Box';
 
 export const AddItem = () => {
     return (            
-        <Box flex>
-            <Box
-                direction='row'
-                align='start'
-                justify='center'
-                colorIndex='light-2'>
-                <Box
-                    alignSelf='center'
-                    margin='large'
-                    colorIndex='light-1'> 
-                    <MealForm />
+        <Box full>
+            <Box flex 
+                align='center'
+                direction='row'>
+                <Box basis='1/2'>
+                    <Box
+                        alignSelf='center'
+                        margin='large'
+                        colorIndex='light-1'> 
+                        <MealForm />
+                    </Box>
                 </Box>
-                <Box
-                    alignSelf='center'
-                    margin='large'
-                    colorIndex='light-1'> 
-                    <SymptomForm />
+                <Box basis='1/2'>
+                    <Box
+                        alignSelf='center'
+                        margin='large'
+                        colorIndex='light-1'> 
+                        <SymptomForm />
+                    </Box>
                 </Box>
             </Box>
             
             <Box flex
                 justify='center'
                 colorIndex='light-2'>
-                <h2>Recent</h2> 
+                <h2>Recent:</h2> 
                 <Box direction='row'
                     justify='center'
                     colorIndex='light-2'>
@@ -39,22 +41,3 @@ export const AddItem = () => {
         </Box>
     )
 }
-
-{/*<Box full={true}>
-  <Box tag='header' direction='row' justify='between' align='center'>
-
-  </Box>
-  <Box flex='grow' direction='row'>
-    <Box align='center'>
-
-    </Box>
-    <Box flex={true} direction='row'>
-      <Box basis='1/2'>
-      
-      </Box>
-      <Box basis='1/2'>
-      
-      </Box>
-    </Box>
-  </Box>
-</Box>*/}

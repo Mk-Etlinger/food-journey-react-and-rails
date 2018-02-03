9ab8fe59bee3119bc73f0bfadfa13e6659d97faf
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-03 11:56:38 -0500

    added Switch to handle routes

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from '../components/home/Home';
 import User from '../components/home/User';
-import Dashboard from '../components/dashboard/Dashboard';
+import ListContainer from '../components/listView/ListContainer';
+import DataContainer from '../components/dataView/DataContainer';
+import RelationContainer from '../components/relationView/RelationContainer';
+import { AddItem } from '../components/addItem/AddItem';
 import { DefaultLayout } from './DefaultLayout';
 import { LoggedInLayout } from './LoggedInLayout';
 
@@ -11,7 +14,12 @@ export default () => (
         <div>
             <DefaultLayout exact path='/' component={ Home } />
             <DefaultLayout path='/login' component={ User } />
-            <LoggedInLayout path='/dashboard' component={ Dashboard } />
+            <Switch>
+                <LoggedInLayout exact path='/dashboard' component={ AddItem } />
+                <LoggedInLayout path='/dashboard/addItem' component={ AddItem } />
+                <LoggedInLayout path='/dashboard/entries' component={ ListContainer } />
+                <LoggedInLayout path='/dashboard/analytics' component={ DataContainer } />
+            </Switch>
         </div>
     </Router>
 )

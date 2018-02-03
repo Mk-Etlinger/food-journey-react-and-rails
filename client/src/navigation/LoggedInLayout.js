085dd8c84d210785ec91c4aeb9041a92981d3e4b
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-03 11:57:17 -0500

    updated with Box and Split from Dashboard

--- client/src/navigation/LoggedInLayout.js
+++ client/src/navigation/LoggedInLayout.js
@@ -1,43 +1,29 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Header from 'grommet/components/Header';
+import { HeaderMenu } from './HeaderMenu';
+import PrimaryNavigation from '../components/dashboard/PrimaryNavigation';
+import MainContent from '../components/dashboard/MainContent';
+import Split from 'grommet/components/Split';
 import Box from 'grommet/components/Box';
-import Menu from 'grommet/components/Menu';
-import Title from 'grommet/components/Title';
-import Anchor from 'grommet/components/Anchor';
-import UserIcon from 'grommet/components/icons/base/User';
 
-export const LoggedInLayout = ({ component: Component, ...props }) => {
+export const LoggedInLayout = ({ component, ...props }) => {
+	
   return (
     <Route { ...props } render={ matchProps => (
       <div className="LoggedInLayout">
-		<Header size='medium'
-			colorIndex='grey-1'>
-			<Title margin={{ left: 'large' }}>
-				<Anchor path='/' >
-					Root Journal
-				</Anchor>
-			</Title>
-			<Box flex 
-				margin={{ right: 'small' }}
-				justify='end'
-				direction='row'
-				responsive >
-				<Menu responsive icon={ <UserIcon /> }
-					dropAlign={{ left: 'left', top: 'bottom' }}>
-					<Anchor href='#' >
-						Settings
-					</Anchor>
-					<Anchor 
-						onClick={ () => localStorage.jwt = '' } 
-						path='/' >
-						Log Out
-					</Anchor>
-				</Menu>
-				
-			</Box>
-		</Header>
-          <Component { ...matchProps } />
+		<HeaderMenu loggedIn={ true } />
+		<Box flex>               
+			<Split flex='right'
+				showOnResponsive='both' >
+				<Box style={{ height: '100%' }} 
+					flex 
+					colorIndex='light-2'>
+					<PrimaryNavigation />
+				</Box>
+					<MainContent { ...matchProps }
+						activeComp={ component } />
+			</Split>
+		</Box>
       </div>
     )} />
   )

efdf0f85661cba171a2344fa0464e902e5dadf7c
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-07 11:04:22 -0500

    removed AddItem, imported DashboardView, MealForm, Symptom Form

--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from '../components/home/Home';
 import User from '../components/home/User';
+import DashboardView from '../components/dashboard/DashboardView';
 import ListContainer from '../components/listView/ListContainer';
 import DataContainer from '../components/dataView/DataContainer';
 import RelationContainer from '../components/relationView/RelationContainer';
-import { AddItem } from '../components/addItem/AddItem';
+import MealForm from '../components/addItem/MealForm';
+import SymptomForm from '../components/addItem/SymptomForm';
 import { DefaultLayout } from './DefaultLayout';
 import { LoggedInLayout } from './LoggedInLayout';
 
@@ -15,10 +17,12 @@ export default () => (
             <DefaultLayout exact path='/' component={ Home } />
             <DefaultLayout path='/login' component={ User } />
             <Switch>
-                <LoggedInLayout exact path='/dashboard' component={ AddItem } />
-                <LoggedInLayout path='/dashboard/addItem' component={ AddItem } />
+                <LoggedInLayout exact path='/dashboard' component={ DashboardView } />
+                <LoggedInLayout path='/dashboard/addMeal' component={ MealForm } />
+                <LoggedInLayout path='/dashboard/addAilment' component={ SymptomForm } />
                 <LoggedInLayout path='/dashboard/entries' component={ ListContainer } />
                 <LoggedInLayout path='/dashboard/analytics' component={ DataContainer } />
+                <LoggedInLayout path='/dashboard/relations' component={ RelationContainer } />
             </Switch>
         </div>
     </Router>

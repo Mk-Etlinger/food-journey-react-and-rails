b753986d85bc360101383a80318ed4be3e071985
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-07 11:04:39 -0500

    added toggle modal reducer

--- client/src/reducers/mealFormData.js
+++ client/src/reducers/mealFormData.js
@@ -1,11 +1,14 @@
 const initialState = {
     active: false,
+    isVisible: false,
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case 'TOGGLE_MEAL_FORM':
             return Object.assign({}, state, { active: action.active })
+        case 'TOGGLE_MEAL_MODAL':
+            return Object.assign({}, state, { isVisible: action.isVisible })
         default:
             return state
     }

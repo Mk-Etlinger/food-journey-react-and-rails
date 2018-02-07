9825c1188a25b4fec483f4d4800142b5bd60906b
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-07 10:58:44 -0500

    added List and ListItem from Grommet. Reworking layout

--- client/src/components/addItem/DateDropdown.js
+++ client/src/components/addItem/DateDropdown.js
@@ -6,17 +6,18 @@ import Heading from 'grommet/components/Heading';
 import Header from 'grommet/components/Header';
 import Paragraph from 'grommet/components/Paragraph';
 import Box from 'grommet/components/Box';
+import List from 'grommet/components/List';
+import ListItem from 'grommet/components/ListItem';
 
 export default ({ date, meals = [], symptoms = [] }) => {
-    let showFormatting = symptoms.length !== 0  &&
-        meals.length !== 0
     const mealButtons = meals.map(meal => {
         return <ShowMealModal
+            key={ meal.id }
+            createdAt={ meal.created_at }
             date={ date }
-            key={ meal.id } 
             mealId={ meal.id } 
             meal_type={ meal.meal_type }
-            ingredients={ meal.ingredients.map(ing => ing.name).join(', ') }/>
+            ingredients={ meal.ingredients.map( ing => ing.name ).join(', ') }/>
      })
 
     const symptomButtons = symptoms.map(symptom => {
@@ -29,26 +30,58 @@ export default ({ date, meals = [], symptoms = [] }) => {
             stress_level={ symptom.reactions[0].stress_level }
             notes={ symptom.reactions[0].notes }
             occurred_at={ symptom.reaction_logs[0].occurred_at }
-            ingredients={ symptom.ingredients.map(ing => ing.name).join(', ') }/>
+            ingredients={ symptom.ingredients.map( ing => ing.name ).join(', ') }/>
     })
     
     return (
-        <Tile align='center' 
-            separator='all' >
+        <Box align='center'
+            pad='small'
+            separator='all'>
             <Header size='small'
-            pad={{"horizontal": "small"}}>
-            <Heading tag='h4'
-                strong >
-                { date }
-            </Heading>
+                pad={{ "horizontal": "small" }}>
+                <Heading tag='h3' >
+                    { date }
+                </Heading>
             </Header>
-            <Box pad='small'>
-                <span>
-                { mealButtons }
-                { showFormatting && <span> | </span> }
-                { symptomButtons }
-                </span>
+            <hr style={{ width: '50%', border: 'solid lightGrey', borderWidth: '1px' }}/>
+            <Box direction='row'
+                align='center'>
+                <Box direction='row' 
+                    align='between'>
+                    Meals:
+                </Box>
+                <Box margin={{ left: 'medium' }}>
+                    Symptoms:
+                </Box>
             </Box>
-        </Tile>
+           
+            <Box direction='row'
+                justify='center'
+                pad='small'
+                size='small'>
+                <Box align='center'
+                    basis='1/3'>
+                    { mealButtons }
+                </Box>
+                <Box align='center'
+                    basis='1/3'>
+                    { symptomButtons }
+                </Box> 
+            </Box>
+        </Box>
     )
 }
+
+ // const test = symptoms.map(symptom => {
+    //     return <List>
+    //         <ListItem justify='between'
+    //             separator='horizontal'>
+    //             <span>
+    //             Alan
+    //             </span>
+    //             <span className='secondary'>
+    //             happy
+    //             </span>
+    //         </ListItem>
+    //     </List>
+    // })

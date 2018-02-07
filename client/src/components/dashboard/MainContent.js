05b6179fdc0cc5c3bf81288cb0c530400ebe24b2
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-07 09:54:07 -0500

    connected redux and add modal toggles for meals and symptoms

--- client/src/components/dashboard/MainContent.js
+++ client/src/components/dashboard/MainContent.js
@@ -1,25 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Box from 'grommet/components/Box';
-import { AddItem } from '../addItem/AddItem';
+import Anchor from 'grommet/components/Anchor';
+import DashboardView from './DashboardView';
+import SymptomForm from '../addItem/SymptomForm';
+import MealForm from '../addItem/MealForm';
+import { toggleMealModal } from '../../actions/meals/toggleMealModal';
+import { toggleSymptomModal } from '../../actions/symptoms/toggleSymptomModal';
 import AddCircleIcon from 'grommet/components/icons/base/AddCircle';
 
 const MainContent = ({ activeComp: ActiveComponent, ...props }) => {
     return (
-        <Box style={{ height: '96.5%' }}
+        <Box 
             pad='small'
             colorIndex='light-2'>
-            { ActiveComponent !== AddItem && 
-                <AddCircleIcon style={ addIconStyling } id='addItem'
-                    size='medium' 
-                    onClick={ () => props.history.replace('/dashboard/addItem') } /> }
-            { ActiveComponent && <ActiveComponent /> }
+            { ActiveComponent !== DashboardView &&
+                <Box 
+                    style={ boxStyling }
+                    direction='row'
+                    colorIndex='light-2'
+                    margin={{ botton: 'small' }}>
+                    <Anchor icon={ <AddCircleIcon size='medium' /> }
+                        onClick={ () => props.toggleMealModal( true ) }
+                        label='Meal' /> 
+                    <Anchor style={ anchorStyling } 
+                        onClick={ () => props.toggleSymptomModal( true ) }
+                        icon={ <AddCircleIcon size='medium' /> }
+                        label='Symptom' />
+                </Box>
+            }
+            { ActiveComponent && <ActiveComponent { ...props }/> }
+            <MealForm />
+            <SymptomForm />
         </Box>
     )
 }
 
-const addIconStyling = { 
-    cursor: 'pointer',
-    marginLeft: '4em'
+const boxStyling = { 
+    marginLeft: '4em',
+    zIndex: 1,
 }
 
-export default MainContent;
+const anchorStyling = { 
+    marginLeft: '1em'
+}
+
+export default connect(null, { toggleMealModal, toggleSymptomModal })(MainContent)

70e0e904faf232a41b37fd62f10367536fc5c5cf
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-14 23:01:32 -0500

    cleaned up stale imports, changed Box pad to small from medium

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,16 +4,8 @@ import { toggleSymptomModal } from '../../actions/symptoms/toggleSymptomModal';
 import { toggleMealModal } from '../../actions/meals/toggleMealModal';
 import MainContent from './MainContent';
 import PrimaryNavigation from './PrimaryNavigation';
-import DashboardView from './DashboardView';
-import RecentsContainer from '../addItem/RecentsContainer';
-import Split from 'grommet/components/Split';
 import Box from 'grommet/components/Box';
-import Anchor from 'grommet/components/Anchor';
-import Heading from 'grommet/components/Heading';
-import PieChartIcon from 'grommet/components/icons/base/PieChart';
-import BookIcon from 'grommet/components/icons/base/Book';
-import AddCircleIcon from 'grommet/components/icons/base/AddCircle';
-import InheritIcon from 'grommet/components/icons/base/Inherit';
+import Footer from 'grommet/components/Footer';
 
 class Dashboard extends Component {
     constructor() {
@@ -30,12 +22,11 @@ class Dashboard extends Component {
     }
 
     render() {
-        const { activeComp } = this.props
         return (
             <Box style={{ boxSizing: 'border-box' }} 
                 full
                 colorIndex='light-2'
-                pad='medium'>
+                pad='small'>
                 <MainContent  { ...this.props }/>
                 <PrimaryNavigation { ...this.props }/>
             </Box>

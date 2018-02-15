b3b43dc24cc420d8ff42dd54a7810687d0d4f172
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-14 22:59:22 -0500

    updated name to updateMealFormContainerData

--- client/src/actions/mealForm.js
+++ client/src/actions/mealForm.js
@@ -1,4 +1,4 @@
-export const updateMealFormData = mealFormData => {
+export const updateMealFormContainerData = mealFormData => {
     return {
         type: 'UPDATE_MEAL_DATA',
         mealFormData

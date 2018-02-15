49d40c5e51c9c5bb5203d5236d46ff0f73fe980d
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-14 22:53:05 -0500

    changed Routes import from nav to Routes,

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import Routes from './navigation';
+import Routes from './routes/Routes';
 import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import '../node_modules/grommet-css';
 import './App.css';
-
 
 class App extends Component {
 

8e033dbadfcd4184a5b50d70388d067443a6d754
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-14 22:59:54 -0500

    removed console.log and added toggleUpdateMealModal

--- client/src/actions/meals/toggleMealModal.js
+++ client/src/actions/meals/toggleMealModal.js
@@ -1,7 +1,14 @@
 export const toggleMealModal = isVisible => {
-    console.log( 'action payload', isVisible )
     return {
         type: 'TOGGLE_MEAL_MODAL',
         isVisible
+    }
+}
+
+export const toggleUpdateMealModal = ( isVisible ) => {
+    console.log('toggling update meal', isVisible)
+    return {
+        type: 'TOGGLE_UPDATE_MEAL_MODAL',
+        isVisible,
     }
 }

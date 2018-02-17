2bf3e280d26a5034cbb04ff6239fc1c68a0ed4fe
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-17 08:19:52 -0500

    removed Table Import, added Heading, Header, CheckmarkIcon. Updated layout

--- client/src/components/dataView/DataView.js
+++ client/src/components/dataView/DataView.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Table from 'grommet/components/Table';
 import TableRow from 'grommet/components/TableRow';
 import DisplayChart from '../reusables/DisplayChart';
 import Box from 'grommet/components/Box';
+import Heading from 'grommet/components/Heading';
+import Header from 'grommet/components/Header';
+import CheckmarkIcon from 'grommet/components/icons/base/Checkmark';
 
 export default ({ mostSymptomaticFoods }) => {
     const ingredientList = Object.keys(mostSymptomaticFoods).map((food, i) => 
@@ -13,9 +15,25 @@ export default ({ mostSymptomaticFoods }) => {
     );
     return (
         <div> 
-            <Box margin='large'
-                align='center' >  
-            <DisplayChart mostSymptomaticFoods={ mostSymptomaticFoods } />
+            <Box pad='medium' 
+                direction='row'>
+                <Box 
+                    margin={{ right: 'small' }}
+                    basis='1/4'>
+                    <Box onClick={ () => alert('Coming Soon') } pad='small' className='darkGreenBG borderBoxShadowSmall' >
+                        <Heading align='start' tag='h4'>
+                            <CheckmarkIcon colorIndex='light-1' /> Mark Foods Safe
+                        </Heading>
+                    </Box>
+                </Box>
+
+                <Box pad='medium'
+                    className='borderBoxShadowSmall'
+                    colorIndex='light-1'
+                    align='center'
+                    basis='3/4' >
+                    <DisplayChart mostSymptomaticFoods={ mostSymptomaticFoods } />
+                </Box>
             </Box>
         </div>
     );

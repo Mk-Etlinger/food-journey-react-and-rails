8d8a9639b5b0a274515289bb30b52b43371b262e
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-19 10:25:42 -0500

    removed stale bind(this) in constructor, changed handleClick to handleOnActive

--- client/src/components/dataView/DataContainer.js
+++ client/src/components/dataView/DataContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import DataView from './DataView'
+import DataSubPageView from './DataSubPageView';
 import { connect } from 'react-redux';
 import { getMeals } from '../../actions/meals/meals';
 import { getSymptoms } from '../../actions/symptoms/symptoms';
 import { getMostSymptomaticFoods } from '../../actions/getMostSymptomaticFoods';
+import Heading from 'grommet/components/Heading';
 import Tabs from 'grommet/components/Tabs';
 import Tab from 'grommet/components/Tab';
 import Box from 'grommet/components/Box';
@@ -16,11 +18,9 @@ class DataContainer extends Component {
             ActiveComp: DataView,
             activeIndex: 0
         }
-
-        this.handleClick = this.handleClick.bind(this)
     }
 
-    handleClick = (e) => {
+    handleOnActive = (e) => {
         const component = componentList[e]
         this.setState({
             ActiveComp: component,
@@ -38,25 +38,29 @@ class DataContainer extends Component {
         const { meals } = this.props.meals, 
             { symptoms } = this.props.symptoms,
             { mostSymptomaticFoods } = this.props.overviewQueries,
-            { ActiveComp } = this.state
+            { ActiveComp, activeIndex } = this.state
         const isMealLoaded = meals.length > 0,
             isSymptomLoaded = symptoms.length > 0,
             isMostSymptomaticFoodsLoaded = Object.keys(mostSymptomaticFoods).length > 0
 
         return (
             <Box flex
-                align='center'>
-                <Box size={{ width: 'xxlarge', height: 'large'}}
-                    colorIndex='light-2'>
-                    <Tabs
-                        activeIndex={ this.state.activeIndex } 
-                        onActive={ this.handleClick }>
-                        <Tab id='IndexView' title='Top Triggers'>
-                            { isMostSymptomaticFoodsLoaded && <ActiveComp 
-                                mostSymptomaticFoods={ mostSymptomaticFoods }/> 
-                            }
-                        </Tab>
-                    </Tabs>
+                align='center'
+                margin='small'
+                colorIndex='light-2'>
+                <Box style={{ marginRight: '10em' }}
+                    alignSelf='center'>
+                    <Heading strong>
+                        Analyze
+                    </Heading>
+                </Box>
+                <Box size={{ width: 'xxlarge', height: 'large'}}>
+                    <Box colorIndex='light-2'>
+                        <DataSubPageView ActiveComp={ ActiveComp }
+                            onActiveCB={ this.handleOnActive }
+                            activeIndex={ activeIndex }
+                            mostSymptomaticFoods={ mostSymptomaticFoods } />
+                    </Box>
                 </Box>
             </Box>
         )

e2623eb2fe98dedd4394f2643a2aa8cdee9d5f63
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-19 12:52:15 -0500

    commented out omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 
-            ENV["FACEBOOK_API_KEY"], 
-            ENV["FACEBOOK_API_SECRET"], 
-            callback_url: "http://localhost:3001/auth/facebook/callback"
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, 
+#             ENV["FACEBOOK_API_KEY"], 
+#             ENV["FACEBOOK_API_SECRET"], 
+#             callback_url: "http://localhost:3001/auth/facebook/callback"
+# end

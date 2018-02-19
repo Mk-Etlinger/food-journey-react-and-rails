a11bd52a1f2399cd9d961c73e13eb0eea9a47a86
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-19 10:29:57 -0500

    added success messages for meals and symptoms

--- client/src/components/recents/RecentsContainer.js
+++ client/src/components/recents/RecentsContainer.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import DateDropdown from './DateDropdown';
 import { getRecentMeals } from '../../actions/meals/recentMeals';
 import { toggleUpdateMealModal } from '../../actions/meals/toggleMealModal';
+import { toggleMealSuccessMessage } from '../../actions/meals/createMeal';
+import { toggleSymptomSuccessMessage } from '../../actions/symptoms/createSymptom';
 import { getRecentSymptoms } from '../../actions/symptoms/recentSymptoms';
 import { connect } from 'react-redux';
 import * as customGroupByDate from '../utilities/CustomGroupByDate';
 import { mergeObjectsByDate } from '../utilities/MergeObjectsByDate';
+import { GrommetStatus } from '../reusables/GrommetStatus';
 import Box from 'grommet/components/Box';
 import Heading from 'grommet/components/Heading';
 import moment from 'moment';
@@ -24,7 +27,11 @@ class RecentsContainer extends Component {
 
     render() {
         const { recentMeals } = this.props.meals, 
-            { recentSymptoms } = this.props.symptoms
+            { recentSymptoms } = this.props.symptoms,
+            { showMealSuccessMessage, 
+                showSymptomSuccessMessage, 
+                toggleMealSuccessMessage,
+                toggleSymptomSuccessMessage } = this.props
         
         let symptomsByDate = recentSymptoms.customGroupByDate(),
             mealsByDate = recentMeals.customGroupByDate()
@@ -41,15 +48,33 @@ class RecentsContainer extends Component {
         
         return (
             <Box className='lightGreenBG'>
-            { mappedObjectsByDate.length ?
-                <Heading margin='small' tag='h3'>
-                    Recent Entries
-                </Heading>
-            :
-                <Heading margin='small' tag='h3'>
-                    No Recent Entries
-                </Heading>
-            }
+                <Box onClick={ () => toggleMealSuccessMessage( false ) } 
+                    direction='row' 
+                    justify='start'>
+                    { showMealSuccessMessage &&
+                        <GrommetStatus onClick={ () => toggleMealSuccessMessage( false ) }
+                            status='ok' 
+                            message='Success!' />
+                    }
+                </Box>
+                <Box onClick={ () => toggleSymptomSuccessMessage( false ) } 
+                    direction='row' 
+                    justify='start'>
+                    { showSymptomSuccessMessage &&
+                        <GrommetStatus onClick={ () => toggleSymptomSuccessMessage( false ) }
+                            status='ok' 
+                            message='Success!' />
+                    }
+                </Box>
+                { mappedObjectsByDate.length ?
+                    <Heading margin='small' tag='h3'>
+                        Recent Entries
+                    </Heading>
+                :
+                    <Heading margin='small' tag='h3'>
+                        No Recent Entries
+                    </Heading>
+                }
             <Box className='lightGreenBG' 
                 direction='row'
                 flex
@@ -71,11 +96,15 @@ const mapStateToProps = (state) => {
     return ({
         meals: state.meals,
         symptoms: state.symptoms,
+        showMealSuccessMessage: state.meals.showMealSuccessMessage,
+        showSymptomSuccessMessage: state.symptoms.showSymptomSuccessMessage,
     })
 }
 
 export default connect(mapStateToProps, { 
     getRecentMeals,
     getRecentSymptoms,
+    toggleMealSuccessMessage,
+    toggleSymptomSuccessMessage,
     toggleUpdateMealModal
 } )(RecentsContainer);

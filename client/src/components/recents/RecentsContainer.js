cebdb4135e189c910dabc31fa4e77291d3487cdf
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-22 12:54:37 -0500

    updated imports, abstracted out get requests to `fetchData`

--- client/src/components/recents/RecentsContainer.js
+++ client/src/components/recents/RecentsContainer.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import DateDropdown from './DateDropdown';
-import { getRecentMeals } from '../../actions/meals/recentMeals';
+import DisplayMessageHelper from './DisplayMessageHelper';
+import { getRecentMeals, isFetchingMeals } from '../../actions/meals/recentMeals';
+import { getRecentSymptoms, isFetchingSymptoms } from '../../actions/symptoms/recentSymptoms';
 import { toggleUpdateMealModal } from '../../actions/meals/toggleMealModal';
 import { toggleMealSuccessMessage } from '../../actions/meals/createMeal';
 import { toggleSymptomSuccessMessage } from '../../actions/symptoms/createSymptom';
-import { getRecentSymptoms } from '../../actions/symptoms/recentSymptoms';
 import { connect } from 'react-redux';
 import { customGroupByDate } from '../utilities/CustomGroupByDate';
 import { mergeObjectsByDate } from '../utilities/MergeObjectsByDate';
-import { GrommetStatus } from '../reusables/GrommetStatus';
 import Box from 'grommet/components/Box';
 import Heading from 'grommet/components/Heading';
 import moment from 'moment';
@@ -17,87 +17,91 @@ import moment from 'moment';
 class RecentsContainer extends Component {
 
     componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = () => {
         this.props.getRecentMeals();
         this.props.getRecentSymptoms();
     }
 
     handleOpenMealModal = ( isVisible, mealId ) => {
-        this.props.toggleUpdateMealModal( isVisible, mealId )
+        this.props.toggleUpdateMealModal( isVisible, mealId );
     }
 
     render() {
-        const { recentMeals } = this.props.meals, 
-            { recentSymptoms } = this.props.symptoms,
-            { showMealSuccessMessage, 
+        const { showMealSuccessMessage, 
                 showSymptomSuccessMessage, 
                 toggleMealSuccessMessage,
-                toggleSymptomSuccessMessage } = this.props
-        
-        let symptomsByDate = customGroupByDate( recentSymptoms ),
-            mealsByDate = customGroupByDate( recentMeals )
+                toggleSymptomSuccessMessage,
+                isFetchingMeals,
+                isFetchingSymptoms } = this.props
+    
+        let symptomsByDate = customGroupByDate( this.props.recentSymptoms ),
+            mealsByDate = customGroupByDate( this.props.recentMeals )
 
         const mergedMealsAndSymptoms = mergeObjectsByDate( mealsByDate, symptomsByDate )
         
-        let mappedObjectsByDate = Object.keys( mergedMealsAndSymptoms ).slice(0, 3).map(( date, i ) => {
+        let mappedObjectsByDate = Object.keys( mergedMealsAndSymptoms ).slice( 0, 3 ).map(( date, i ) => {
             return <DateDropdown key={ i } 
                 showUpdateModal={ this.props.updateModalIsVisible }
                 date={ moment(date, 'YYYY-MM-DD').format('MMM Do') }
                 symptoms={ mergedMealsAndSymptoms[date].symptoms } 
                 meals={ mergedMealsAndSymptoms[date].meals } />
         })
-        
+        console.log(this.props.isFetchingSymptoms)
         return (
             <Box className='lightGreenBG'>
-                <Box onClick={ () => toggleMealSuccessMessage( false ) } 
-                    direction='row' 
-                    justify='start'>
-                    { showMealSuccessMessage &&
-                        <GrommetStatus onClick={ () => toggleMealSuccessMessage( false ) }
-                            status='ok' 
-                            message='Success!' />
-                    }
-                </Box>
-                <Box onClick={ () => toggleSymptomSuccessMessage( false ) } 
-                    direction='row' 
-                    justify='start'>
-                    { showSymptomSuccessMessage &&
-                        <GrommetStatus onClick={ () => toggleSymptomSuccessMessage( false ) }
-                            status='ok' 
-                            message='Success!' />
-                    }
-                </Box>
+                <DisplayMessageHelper showMessage={ showMealSuccessMessage }
+                    onClickCB={ () => toggleMealSuccessMessage( false ) }
+                    status='ok' 
+                    message='Success!' />
+
+                <DisplayMessageHelper showMessage={ showSymptomSuccessMessage }
+                    onClickCB={ () => toggleSymptomSuccessMessage( false ) }
+                    status='ok' 
+                    message='Success!' />
+                    
                 { mappedObjectsByDate.length ?
                     <Heading margin='small' tag='h3'>
                         Recent Entries
                     </Heading>
                 :
                     <Heading margin='small' tag='h3'>
-                        No Recent Entries
+                        { isFetchingMeals && isFetchingSymptoms ?
+                            'Loading...'
+                        :
+                            'No Recent Entries'
+                        }
                     </Heading>
                 }
-            <Box className='lightGreenBG' 
-                direction='row'
-                flex
-                wrap
-                size={{ height: { max: 'xlarge' }, width: { min: 'medium', max: 'xlarge' } }}> 
-                { mappedObjectsByDate }
-            </Box>
+                <Heading margin='small' tag='h3'>
+                </Heading>
+                <Box className='lightGreenBG' 
+                    direction='row'
+                    flex
+                    wrap
+                    size={{ height: { max: 'xlarge' }, width: { min: 'medium', max: 'xlarge' } }}> 
+                    { mappedObjectsByDate }
+                </Box>
             </Box>
         )
     }
 }
 
 RecentsContainer.defaultProps = {
-    recentMeals: {},
-    recentSymptoms: {},
+    recentMeals: [],
+    recentSymptoms: [],
 }
 
 const mapStateToProps = (state) => {
     return ({
-        meals: state.meals,
-        symptoms: state.symptoms,
+        recentSymptoms: state.symptoms.recentSymptoms,
+        recentMeals: state.meals.recentMeals,
         showMealSuccessMessage: state.meals.showMealSuccessMessage,
         showSymptomSuccessMessage: state.symptoms.showSymptomSuccessMessage,
+        isFetchingMeals: state.fetch.isFetchingMeals,
+        isFetchingSymptoms: state.fetch.isFetchingSymptoms
     })
 }
 

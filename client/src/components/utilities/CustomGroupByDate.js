e85f060a56996f8095713a7b1f6132fd0cf795a8
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-21 20:35:03 -0500

    removed function from Array.prototype

--- client/src/components/utilities/CustomGroupByDate.js
+++ client/src/components/utilities/CustomGroupByDate.js
@@ -1,10 +1,20 @@
 import moment from 'moment';
 
-Array.prototype.customGroupByDate = function() {
-    return this.reduce( function(key, item){
-        let date = moment(item.created_at).format('Y-M-D')
-        key[date] = key[date] || [];
-        key[date].push(item);
+export const customGroupByDate = ( array ) => {
+    return array.reduce(( key, item ) => {
+        let date = moment( item.created_at ).format( 'Y-M-D' )
+        key[ date ] = key[ date ] || [];
+        key[ date ].push( item );
         return key
     }, {})
 }
+
+// Don't use this!
+// Array.prototype.customGroupByDate = function() {
+//     return this.reduce( function(key, item){
+//         let date = moment(item.created_at).format('Y-M-D')
+//         key[date] = key[date] || [];
+//         key[date].push(item);
+//         return key
+//     }, {})
+// }
